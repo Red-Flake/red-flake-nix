ea1af68eb487283a2246edf5027137f677754c00
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-24 21:50:20 +0000

    refactor packages

--- flake.nix
+++ flake.nix
@@ -1,44 +1,44 @@
 {
-  description = "Red-Flake";
+  description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # Third party programs, packaged with nix
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hardware.url = "github:nixos/nixos-hardware";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, hardware, ... } @ inputs: let
-    inherit (nixpkgs.lib) genAttrs;
-    systems = ["x86_64-linux"];  # Add other systems as needed
-    pkgsFor = genAttrs systems (system: import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    });
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
   in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#default'
     nixosConfigurations = {
-      default = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./hosts/default/default.nix ];
-        specialArgs = { inherit inputs; };
+      # FIXME replace with your hostname
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./nixos/configuration.nix];
       };
     };
 
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "pascal@default" = home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-linux";
-        modules = [ hosts/default/home.nix ];
-        pkgs = pkgsFor.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
+      # FIXME replace with your username@hostname
+      "pascal@nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home-manager/home.nix];
       };
     };
   };
 }
-
