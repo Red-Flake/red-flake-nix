49479dead36d503c31df0e15f5619e04661cf93d
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-24 20:57:27 +0200

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,12 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager,... } @ inputs: {
+  outputs = { 
+    self, 
+    nixpkgs, 
+    home-manager,
+    ... 
+  } @ inputs: {
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
