bdc46892d928dda7995a0adb0e147d5e675574e4
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-25 01:21:03 +0000

    home-manager: add firefox addons

--- flake.nix
+++ flake.nix
@@ -6,8 +6,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,6 +14,9 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
+    #
+    # import firefox.nix
+    ./modules/firefox.nix
   ];
 
   nixpkgs = {
