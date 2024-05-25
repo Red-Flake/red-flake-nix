fc3047bd51b85cad2046d9b302551e554a15f3b1
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-25 00:39:25 +0000

    modularize user config

---
+++ nixos/users.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+# TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.users = {
+    # FIXME: Replace with your username
+    pascal = {
+      # TODO: You can set an initial password for your user.
+      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
+      # Be sure to change it (using passwd) after rebooting!
+      initialPassword = "correcthorsebatterystaple";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = [
+        "wheel"
+        "sudo"
+        "uucp"
+        "lp"
+        "adm"
+      ];
+    };
+  };
+}
