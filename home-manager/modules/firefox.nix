e80d1f92d269b6ae67cda2d8772b28657cfeff4c
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-25 01:21:03 +0000

    home-manager: add firefox addons

---
+++ home-manager/modules/firefox.nix
@@ -0,0 +1,36 @@
+# firefox.nix
+
+{ config, pkgs, inputs, ... }:
+
+{
+  programs.firefox = {
+    enable = true;
+
+    profiles.pascal = {
+
+      bookmarks = [
+        {
+          name = "RedFlake";
+          tags = [ "redflake" ];
+          keywords = "redflake";
+          url = "https://github.com/red-Flake/";
+        }
+      ];
+
+      settings = {
+        "extensions.update.autoUpdateDefault" = false;
+      };
+
+      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        foxyproxy-standard
+        dracula-dark-colorscheme
+        darkreader
+        wappalyzer
+        waybackmachine
+        ublock-origin        
+      ];
+
+    };
+  };
+
+}
