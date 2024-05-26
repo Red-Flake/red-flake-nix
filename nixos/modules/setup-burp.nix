cdea6e6da93e6b19b37b508c39cf97590c8a2e34
MagicBytes <ppeinecke@protonmail.com>
2024-05-26 07:04:39 +0200

    try to fix writeShellScriptBin

--- nixos/modules/setup-burp.nix
+++ nixos/modules/setup-burp.nix
@@ -1,9 +1,8 @@
-{ pkgs }:
+{ pkgs, ... }:
 
-{
-    pkgs.writeShellScriptBin setup-burp ''
+let
+   setup-burp = pkgs.writeShellScriptBin "setup-burp" ''
         ${pkgs.bash}/bin/bash -c "${pkgs.coreutils}/bin/timeout 45 ${pkgs.burpsuite}/bin/burpsuite < <(${pkgs.coreutils}/bin/echo y) &"
         ${pkgs.coreutils}/bin/sleep 20
         ${pkgs.curl}/bin/curl http://localhost:8080/cert -o /etc/ssl/certs/BurpSuiteCA.der
     '';
-}
