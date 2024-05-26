93358d384ecb5810f6d0372c69db152d92e88910
MagicBytes <ppeinecke@protonmail.com>
2024-05-26 11:25:23 +0200

    try to fix system.activationScripts

--- nixos/modules/setup-burp.nix
+++ nixos/modules/setup-burp.nix
@@ -1,15 +1,9 @@
 { pkgs, ... }:
 
-let
-    setup-burp = pkgs.writeShellScriptBin "setup-burp" ''
-        ${pkgs.bash}/bin/bash -c "${pkgs.coreutils}/bin/timeout 45 ${pkgs.burpsuite}/bin/burpsuite < <(${pkgs.coreutils}/bin/echo y) &"
+{
+    system.activationScripts.setup-burp.text = ''
+      ${pkgs.bash}/bin/bash -c "${pkgs.coreutils}/bin/timeout 45 ${pkgs.burpsuite}/bin/burpsuite < <(${pkgs.coreutils}/bin/echo y) &"
         ${pkgs.coreutils}/bin/sleep 20
         ${pkgs.curl}/bin/curl http://localhost:8080/cert -o /etc/ssl/certs/BurpSuiteCA.der
     '';
-in {
-    environment.systemPackages = [ setup-burp ];
 }
-
-system.activationScripts.setup-burp.text = ''
-    setup-burp
-'';
