3d30d1a6e051693bddf5fa5f918a496e23061359
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-27 07:16:39 +0200

    try to fix zsh

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -1,32 +1,49 @@
-{ pkgs, ... }:
-
+{ config, lib, pkgs, ... }:
+let
+  # load the configuration that we was generated the first
+  # time zsh were loaded with powerlevel enabled.
+  # Make sure to comment this part (and the sourcing part below)
+  # before you ran powerlevel for the first time or if you want to run
+  # again 'p10k configure'. Then, copy the generated file as:
+  # $ mv ~/.p10k.zsh p10k-config/p10k.zsh
+  configThemeNormal = ./p10k-config/p10k.zsh;
+  configThemeTTY = ./p10k-config/p10k_tty.zsh;
+in
 {
-    programs.zsh = {
-
-       enable = true;
-
-       enableCompletion = true;
-       autosuggestion.enable = true;
-       syntaxHighlighting.enable = true;
-       shellAliases = {
-            ll = "ls -l";
-            update = "sudo nixos-rebuild switch";
-       };
-
-        oh-my-zsh = {
-            enable = true;
-            theme = "powerlevel10k";
-            plugins = [
-                "git"
-                "npm"
-                "history"
-                "node"
-                "rust"
-                "deno"
-                "powerlevel10k"
-            ];
-        };
-
-    };
-
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    # Meslo Nerd Font patched for Powerlevel10k
+    # Restart Konsole and configure it (profile) to choose MesloLGS NF
+    meslo-lgs-nf
+  ];
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      # The powerlevel theme I'm using is distgusting in TTY, let's default
+      # to something else
+      # See https://github.com/romkatv/powerlevel10k/issues/325
+      # Instead of sourcing this file you could also add another plugin as
+      # this, and it will automatically load the file for us
+      # (but this way it is not possible to conditionally load a file)
+      # {
+      #   name = "powerlevel10k-config";
+      #   src = lib.cleanSource ./p10k-config;
+      #   file = "p10k.zsh";
+      # }
+      if zmodload zsh/terminfo && (( terminfo[colors] >= 256 )); then
+        [[ ! -f ${configThemeNormal} ]] || source ${configThemeNormal}
+      else
+        [[ ! -f ${configThemeTTY} ]] || source ${configThemeTTY}
+      fi
+    '';
+    plugins = [
+      {
+        # A prompt will appear the first time to configure it properly
+        # make sure to select MesloLGS NF as the font in Konsole
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+  };
 }
