4792c6f4bb9bbeaeb565c9505c70653152632a8c
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-27 06:03:40 +0200

    try to fix zsh home-manager config

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -1,22 +1,26 @@
-programs.zsh = {
-   enable = true;
-   enableAutosuggestions.enable = true;
-   syntaxHighlighting.enable = true;
-   shellAliases = {
-    ll = "ls -l";
-    update = "sudo nixos-rebuild switch";
-  };
-};
+{ pkgs, ... }:
 
-programs.oh-my-zsh = {
-    enable = true;
-    theme = "agnoster";
-    plugins = [
-        "git"
-        "npm"
-        "history"
-        "node"
-        "rust"
-        "deno"
-    ];
+{
+    programs.zsh = {
+       enable = true;
+       enableAutosuggestions.enable = true;
+       syntaxHighlighting.enable = true;
+       shellAliases = {
+        ll = "ls -l";
+        update = "sudo nixos-rebuild switch";
+      };
+    };
+    
+    programs.oh-my-zsh = {
+        enable = true;
+        theme = "agnoster";
+        plugins = [
+            "git"
+            "npm"
+            "history"
+            "node"
+            "rust"
+            "deno"
+        ];
+    };
 };
