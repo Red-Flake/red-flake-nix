03d64fdf2d5224bfb920422bb5f680b50bb5b049
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-27 06:13:05 +0200

    try to fix oh-my-zsh

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -2,25 +2,28 @@
 
 {
     programs.zsh = {
+        
        enable = true;
        enableAutosuggestions.enable = true;
        syntaxHighlighting.enable = true;
        shellAliases = {
-        ll = "ls -l";
-        update = "sudo nixos-rebuild switch";
-      };
+            ll = "ls -l";
+            update = "sudo nixos-rebuild switch";
+       };
+
+        oh-my-zsh = {
+            enable = true;
+            theme = "agnoster";
+            plugins = [
+                "git"
+                "npm"
+                "history"
+                "node"
+                "rust"
+                "deno"
+            ];
+        };
+
     };
 
-    programs.oh-my-zsh = {
-        enable = true;
-        theme = "agnoster";
-        plugins = [
-            "git"
-            "npm"
-            "history"
-            "node"
-            "rust"
-            "deno"
-        ];
-    };
 }
