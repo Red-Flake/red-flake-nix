e31f20153d8b8d52336c552e13f567fc0e4fb1c4
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-27 07:41:41 +0200

    zsh: add environment.pathsToLink to get completion for system packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -57,6 +57,7 @@
   # set zsh as default shell:
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
+  environment.pathsToLink = [ "/share/zsh" ];
   programs.zsh.enable = true;
 
   system.stateVersion = "23.05";
