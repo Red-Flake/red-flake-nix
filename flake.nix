c759ec4777d46bb85e02ceaa3a3afba39740fa17
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-27 05:33:08 +0200

    try to fix home manager install

--- flake.nix
+++ flake.nix
@@ -14,17 +14,7 @@
     NUR.url = "github:nix-community/NUR";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: 
-  let
-    unfreePkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-    };
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
     username = "pascal";
     hostname = "nixos";
     system = "x86_64-linux";
@@ -32,32 +22,39 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#default'
     nixosConfigurations = {
-      # FIXME replace with your hostname
+      # Replace with your hostname
       redflake = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
-        # > Our main nixos configuration file <
+        inherit system;
+        specialArgs = { inherit inputs; };
+        # Main NixOS configuration file
         modules = [
           ./nixos/configuration.nix
-          home-manager.nixosModules.home-manager {
+          { 
+            # Home Manager integration as a NixOS module
+            imports = [ inputs.home-manager.nixosModules.home-manager ];
+
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.pascal = import ./home-manager/home.nix;
-            home-manager.extraSpecialArgs = {inherit inputs username; };
+            home-manager.users = {
+              pascal = { pkgs, ... }: {
+                home = import ./home-manager/home.nix { inherit pkgs; };
+              };
+            };
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
-	        }
+            home-manager.extraSpecialArgs = { inherit inputs username; };
+          }
         ];
       };
     };
-  };
 
-  homeConfigurations.pascal = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
+    homeConfigurations.pascal = home-manager.lib.homeManagerConfiguration {
+      inherit system;
+      pkgs = import nixpkgs { inherit system; };
       extraSpecialArgs = { inherit inputs; };
       modules = [ 
         ./home-manager/home.nix
       ];
+    };
   };
-
 }
