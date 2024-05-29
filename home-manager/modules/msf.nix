ab89dadc6656deec6150fbb348c0d609877efe9f
Mag1cByt3s <ppeinecke@protonmail.com>
2024-05-29 04:00:46 +0200

    home-manager: add msf db config

---
+++ home-manager/modules/msf.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+{
+    home.file.".msf4/database.yml".text = ''
+        # To set up a metasploit database, follow the directions hosted at:
+        # http://r-7.co/MSF-DEV#set-up-postgresql
+        development: &pgsql
+            adapter: postgresql
+            database: msfdb 
+            username: msfuser
+            password: msfpass 
+            host: localhost
+            port: 5432
+            pool: 200
+            timeout: 5
+
+        # You will often want to seperate your databases between dev
+        # mode and prod mode. Absent a production db, though, defaulting
+        # to dev is pretty sensible for many developer-users.
+        production: &production
+          <<: *pgsql
+    '';
+}
