67065dc8b92a46ebad79372b25c6315f9d70038b
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-02 16:11:53 +0200

    add shell env for c/c++

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -101,6 +101,8 @@ in
                     python2-shell = "NIXPKGS_ALLOW_INSECURE=1 nix-shell -p python2";
                     ruby-shell = "nix-shell -p ruby bundler";
                     node-shell = "nix-shell -p nodePackages_latest.nodejs";
+                    c-shell = "nix-shell -p gcc gnumake cmake";
+                    cpp-shell ="c-shell";
                 };
 
                 # define session variables
