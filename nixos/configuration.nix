33e38f3b0c4c442153d43ab081c89fbd0991990f
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-02 15:34:53 +0200

    try to fix broken nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -29,19 +29,44 @@
   ];
 
   nixpkgs = {
-    overlays = [];
-    config.allowUnfree = true;
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
   };
 
-  nix = {
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
     settings = {
+      # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
       flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+      # Enable deduplication
       auto-optimise-store = true;
     };
+    # Opinionated: disable channels
     channel.enable = false;
-    registry = lib.mapAttrs (_: flake: { inherit flake; }) inputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   networking.hostName = "redflake";
