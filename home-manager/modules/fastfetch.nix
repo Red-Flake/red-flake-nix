7508cb29f1bb0b57a036af6e9ab0f954f8fb3e71
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-03 12:52:45 +0200

    try to fix fastfetch settings

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -1,12 +1,47 @@
 { config, lib, pkgs, ... }:
 {
     # https://home-manager-options.extranix.com/?query=fastfetch&release=master
-    programs = {
-          fastfetch = {
-              # enable fastfetch
-              enable = true;
-              settings = '{ "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json", "logo": { "source": "nixos", "color": { "1": "red", "2": "red" }, }, "display": { "color": { "title": "red", "keys": "red" } }, "modules": [ "title", "separator", "os", "host", "kernel", "uptime", "packages", "shell", "de", "wm", "wmtheme", "theme", "icons", "font", "terminal", "terminalfont", "cpu", "gpu", "memory", "swap", "disk", "break" ]; };'
-              };
+    programs.fastfetch = {
+      enable = true;
+      settings = {
+        "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+        logo = {
+          source = "nixos";
+          color = {
+            "1" = "red";
+            "2" = "red";
           };
-    };
+        };
+        display = {
+          color = {
+            title = "red";
+            keys = "red";
+          };
+        };
+        modules = [
+          "title"
+          "separator"
+          "os"
+          "host"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          "de"
+          "wm"
+          "wmtheme"
+          "theme"
+          "icons"
+          "font"
+          "terminal"
+          "terminalfont"
+          "cpu"
+          "gpu"
+          "memory"
+          "swap"
+          "disk"
+          "break"
+        ];
+      };
+  };
 }
