4dec1437b1a39d71a34ddbef4fb4aad718a010f9
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-03 12:46:21 +0200

    try to fix fastfetch settings

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -5,46 +5,7 @@
           fastfetch = {
               # enable fastfetch
               enable = true;
-              settings = {
-                  "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
-                  "logo": {
-                    "source": "nixos",
-                    "color": {
-                      "1": "red",
-                      "2": "red"
-                    },
-                  },
-                  "display": {
-                    "color": {
-                      "title": "red",
-                      "keys": "red"      
-                    }
-                  },
-                  "modules": [
-                    "title",
-                    "separator",
-                    "os",
-                    "host",
-                    "kernel",
-                    "uptime",
-                    "packages",
-                    "shell",
-                    "de",
-                    "wm",
-                    "wmtheme",
-                    "theme",
-                    "icons",
-                    "font",
-                    "terminal",
-                    "terminalfont",
-                    "cpu",
-                    "gpu",
-                    "memory",
-                    "swap",
-                    "disk",
-                    "break"
-                  ]
-                }
+              settings = '{ "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json", "logo": { "source": "nixos", "color": { "1": "red", "2": "red" }, }, "display": { "color": { "title": "red", "keys": "red" } }, "modules": [ "title", "separator", "os", "host", "kernel", "uptime", "packages", "shell", "de", "wm", "wmtheme", "theme", "icons", "font", "terminal", "terminalfont", "cpu", "gpu", "memory", "swap", "disk", "break" ]; };'
               };
           };
     };
