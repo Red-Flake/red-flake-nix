c89642f17177703e0c728be1c74d016639a475d0
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-04 11:29:11 +0200

    neo4j settings got renamed

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -42,8 +42,8 @@
   services.neo4j = {
     enable = true;
     directories.home = "/var/lib/neo4j";
-    enableBolt = true;
-    enableHttps = true;
+    bolt.enable = true;
+    https.enable = true;
     http.listenAddress = ":7474";
     https.listenAddress = ":7473";
     bolt.listenAddress = ":7687";
