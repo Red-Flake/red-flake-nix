2f6e0c0ae0d10796f44096fe6bc8193e365de452
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-05 10:34:21 +0200

    increase timeouts for burp setup script to make sure burp is fully started before downloading the certificate

--- nixos/modules/setup-burp.nix
+++ nixos/modules/setup-burp.nix
@@ -5,8 +5,8 @@
   system.activationScripts.setup-burp = {
     text = ''
       if ! [ -f /etc/ssl/certs/BurpSuiteCA.der ]; then
-        ${pkgs.bash}/bin/bash -c "${pkgs.coreutils}/bin/timeout 45 ${pkgs.burpsuite}/bin/burpsuite < <(${pkgs.coreutils}/bin/echo y) &"
-        ${pkgs.coreutils}/bin/sleep 20
+        ${pkgs.bash}/bin/bash -c "${pkgs.coreutils}/bin/timeout 60 ${pkgs.burpsuite}/bin/burpsuite < <(${pkgs.coreutils}/bin/echo y) &"
+        ${pkgs.coreutils}/bin/sleep 40
         ${pkgs.curl}/bin/curl http://localhost:8080/cert -o /etc/ssl/certs/BurpSuiteCA.der
       fi
     '';
