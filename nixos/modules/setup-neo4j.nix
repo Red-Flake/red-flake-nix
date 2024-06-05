f3ef5bdb978228e5af19e28e43773abd6547422a
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-05 11:20:16 +0200

    forgot '

--- nixos/modules/setup-neo4j.nix
+++ nixos/modules/setup-neo4j.nix
@@ -4,7 +4,7 @@
   # run script to automatically set the neo4j password to Password1337
   system.activationScripts.setup-neo4j = {
     text = ''
-      NEO4J_CONF=/var/lib/neo4j/conf ${lib.getExe pkgs.neo4j "neo4j-admin"} dbms set-initial-password Password1337
+      NEO4J_CONF=/var/lib/neo4j/conf ${lib.getExe' pkgs.neo4j "neo4j-admin"} dbms set-initial-password Password1337
     '';
   };
 }
