e192fcb4499fe35f4b790aeb73d61ab705766b6f
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-05 11:14:50 +0200

    try to fix issues with system.activationScripts.setup-neo4j

--- nixos/modules/setup-neo4j.nix
+++ nixos/modules/setup-neo4j.nix
@@ -4,7 +4,7 @@
   # run script to automatically set the neo4j password to Password1337
   system.activationScripts.setup-neo4j = {
     text = ''
-      ${pkgs.bash}/bin/bash -c "NEO4J_CONF=/var/lib/neo4j/conf/ /run/current-system/sw/bin/neo4j-admin dbms set-initial-password Password1337"
+      NEO4J_CONF=/var/lib/neo4j/conf ${lib.getExe pkgs.neo4j "neo4j-admin"} dbms set-initial-password Password1337
     '';
   };
 }
