4010d09d672204ac552fafafecd521416748c883
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-06 08:48:22 +0200

    fastfetch: test logo file loading

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -6,11 +6,8 @@
       settings = {
         "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
         logo = {
-          source = "nixos";
-          color = {
-            "1" = "red";
-            "2" = "red";
-          };
+          type = "iterm";
+          source = "/home/pascal/artwork/logos/RedFlake_Logo_512x512px.png";
         };
         display = {
           color = {
