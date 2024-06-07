7043048eab342b698767156ada9bd3313033c65f
MagicBytes <48777549+Mag1cByt3s@users.noreply.github.com>
2024-06-07 14:39:29 +0200

    Update bootloader.nix

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -2,7 +2,29 @@
 
 {
   # Enable Grub Bootloader
-  boot.kernelParams = [ "i915.enable_fbc=1", "i915.fastboot=1", "fsck.mode=skip", "loglevel=0", "rd.systemd.show_status=false", "nowatchdog", "kernel.nmi_watchdog=0", "nomce", "mitigations=off", "libahci.ignore_sss=1", "modprobe.blacklist=iTCO_wdt", "modprobe.blacklist=sp5100_tco", "processor.ignore_ppc=1", "sysrq_always_enabled=1", "split_lock_detect=off", "consoleblank=0", "audit=0", "net.ifnames=0", "biosdevname=0" ];
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "i915.enable_fbc=1"
+    "i915.fastboot=1"
+    "fsck.mode=skip"
+    "loglevel=0"
+    "rd.systemd.show_status=false"
+    "nowatchdog"
+    "kernel.nmi_watchdog=0"
+    "nomce"
+    "mitigations=off"
+    "libahci.ignore_sss=1"
+    "modprobe.blacklist=iTCO_wdt"
+    "modprobe.blacklist=sp5100_tco"
+    "processor.ignore_ppc=1"
+    "sysrq_always_enabled=1"
+    "split_lock_detect=off"
+    "consoleblank=0"
+    "audit=0"
+    "net.ifnames=0"
+    "biosdevname=0"
+  ];
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
