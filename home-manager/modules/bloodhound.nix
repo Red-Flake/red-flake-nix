15d2f6462865b5103a81d4bb6a57ccfd37a1ed2a
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-09 06:20:43 +0200

    try to fix json

--- home-manager/modules/bloodhound.nix
+++ home-manager/modules/bloodhound.nix
@@ -1,54 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  home.file.".config/bloodhound/config.json".force = true;
-  home.file.".config/bloodhound/config.json".text = ''
-{
-        "performance": {
-                "edge": 5,
-                "lowGraphics": false,
-                "nodeLabels": 1,
-                "edgeLabels": 1,
-                "darkMode": true
-        },
-        "edgeincluded": {
-                "MemberOf": true,
-                "HasSession": true,
-                "AdminTo": true,
-                "AllExtendedRights": true,
-                "AddMember": true,
-                "ForceChangePassword": true,
-                "GenericAll": true,
-                "GenericWrite": true,
-                "Owns": true,
-                "WriteDacl": true,
-                "WriteOwner": true,
-                "CanRDP": true,
-                "ExecuteDCOM": true,
-                "AllowedToDelegate": true,
-                "ReadLAPSPassword": true,
-                "Contains": true,
-                "GPLink": true,
-                "AddAllowedToAct": true,
-                "AllowedToAct": true,
-                "WriteAccountRestrictions": true,
-                "SQLAdmin": true,
-                "ReadGMSAPassword": true,
-                "HasSIDHistory": true,
-                "CanPSRemote": true,
-                "SyncLAPSPassword": true,
-                "DumpSMSAPassword": true,
-                "AZMGGrantRole": true,
-                "AZMGAddSecret": true,
-                "AZMGAddOwner": true,
-                "AZMGAddMember": true,
-                "AZMGGrantAppRoles": true,
-                "AZNodeResourceGroup": true,
-                "AZWebsiteContributor": true,
-                "AZLogicAppContributo": true,
-                "AZAutomationContributor": true,
-                "AZAKSContributor": true
-        }
-}
-
-  '';
+    home.file.".config/bloodhound/config.json".force = true;
+    home.file.".config/bloodhound/config.json".source = ./bloodhound/config.json;
 }
