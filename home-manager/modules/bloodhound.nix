942a92b1b15c9b1985aeeeb76cc1246a59662a64
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-09 07:11:17 +0200

    issues with home-manager replacing config.json

--- home-manager/modules/bloodhound.nix
+++ home-manager/modules/bloodhound.nix
@@ -1,4 +1,4 @@
 { config, lib, pkgs, ... }: {
-    home.file.".config/bloodhound/config.json".force = true;
+    #home.file.".config/bloodhound/config.json".force = true;
     home.file.".config/bloodhound/config.json".source = ./bloodhound/config.json;
 }
