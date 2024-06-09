f66c9f474ac034468b447d61b9fc5bd9fc1321cd
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-09 12:33:28 +0200

    fix unwritable path

--- home-manager/modules/bloodhound.nix
+++ home-manager/modules/bloodhound.nix
@@ -5,10 +5,11 @@
     source = ./bloodhound/config.json;
     recursive = true;
     onChange = ''
-      cp ${config.xdg.configHome}/bloodhound/config.json /tmp/
+      cp ${config.xdg.configHome}/bloodhound/config.json ${home.homeDirectory}
       rm -f ${config.xdg.configHome}/bloodhound/config.json
-      cp /tmp/config.json ${config.xdg.configHome}/bloodhound/config.json
+      cp ${home.homeDirectory}/config.json ${config.xdg.configHome}/bloodhound/config.json
       chmod u+w ${config.xdg.configHome}/bloodhound/config.json
+      rm -f ${home.homeDirectory}/config.json
     '';
   };
 }
