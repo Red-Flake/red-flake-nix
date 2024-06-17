dbae8b049a019d9510862ed986f3378d41382e7f
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-17 14:45:40 +0200

    fix chaotic-nyx cache

--- flake.nix
+++ flake.nix
@@ -1,30 +1,61 @@
 {
   description = "Red-Flake";
 
+  nixConfig.extra-substituters = [
+    "https://cache.garnix.io"
+    "https://nyx.chaotic.cx"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  ];
+
   inputs = {
-    # chaotic-nyx
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-
-    # NUR
-    NUR.url = "github:nix-community/NUR";
-
-    # nixos-unstable 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    # home-manager
-    home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+    # Chaotic's Nyx
+    chaotic-nyx = {
+      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+      inputs.home-manager.follows = "home-manager";
     };
 
+    # If you need to, override this to use a different nixpkgs version
+    # by default we follow Chaotic Nyx' nyxpkgs-unstable branch
+    nixpkgs.follows = "chaotic-nyx/nixpkgs";
+
+    # Modules support for flakes
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "chaotic-nyx/nixpkgs";
+    };
+
+    # Have a local index of nixpkgs for fast launching of apps
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
+    };
+    
+    # Home configuration management
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
+    };
+
+    # Easy linting of the flake and all kind of other stuff
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-compat.follows = "chaotic-nyx/nixpkgs";
+      inputs.nixpkgs.follows = "chaotic-nyx/nixpkgs";
+      # Only used for the tests of pre-commit-hooks. Override stops double fetch
+      inputs.nixpkgs-stable.follows = "chaotic-nyx/nixpkgs";
+    };
+
+    NUR.url = "github:nix-community/NUR";
   };
 
   outputs = {
-    self,
-    chaotic,
+    flake-parts,
     nixpkgs,
-    NUR,
-    home-manager, 
+    pre-commit-hooks,
     ... 
   } @ inputs: let
     system = "x86_64-linux";
@@ -37,7 +68,6 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./nixos/configuration.nix
-          chaotic.nixosModules.default
           {
             imports = [ inputs.home-manager.nixosModules.home-manager ];
 
@@ -53,6 +83,7 @@
             };
             home-manager.extraSpecialArgs = { inherit inputs username homeDirectory; };
           }
+          
         ];
       };
     };
