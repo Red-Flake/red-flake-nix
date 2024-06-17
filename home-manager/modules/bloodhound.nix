e3b8acff3130d30a003fb2ee11f4c59673a29304
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-17 02:13:05 +0000

    fix bloodhound config creation

--- home-manager/modules/bloodhound.nix
+++ home-manager/modules/bloodhound.nix
@@ -1,22 +1,28 @@
 { config, lib, pkgs, ... }:
 
 let
+  bloodhoundPath = "${config.xdg.configHome}/bloodhound";
   configJsonPath = "${config.xdg.configHome}/bloodhound/config.json";
   sourceConfigJsonPath = "${./bloodhound/config.json}";
 in
 {
   home.activation.preActivation = ''
-    # Ensure the existing config file is removed before activation
-    if [ -f ${configJsonPath} ]; then
-      rm -f ${configJsonPath}
+    # Check if bloodhound folder exists
+    if [ ! -d "${bloodhoundPath}" ]; then
+      # If not, create bloodhound folder
+      mkdir -p "${bloodhoundPath}"
     fi
-
+    
     # Copy the new config file
-    cp ${sourceConfigJsonPath} ${configJsonPath}
+    cp "${sourceConfigJsonPath}" "${configJsonPath}"
   '';
 
   home.activation.postActivate = ''
     # Ensure the config file has the correct permissions
-    chmod u+w ${configJsonPath}
+    if [ -f "${configJsonPath}" ]; then
+      chmod u+w "${configJsonPath}"
+    else
+      echo "Config file not found: ${configJsonPath}"
+    fi
   '';
 }
