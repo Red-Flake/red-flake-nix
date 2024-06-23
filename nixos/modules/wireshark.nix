437eb2459357fdf52e46532f6f0ff38e26f0879d
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-23 20:00:32 +0200

    properly enable wireshark package & add user to wireshark group

---
+++ nixos/modules/wireshark.nix
@@ -0,0 +1,9 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  # Enable Wireshark
+  programs.wireshark.enable = true;
+
+  # Use Wireshark-QT
+  programs.wireshark.package = "pkgs.wireshark";    
+}
