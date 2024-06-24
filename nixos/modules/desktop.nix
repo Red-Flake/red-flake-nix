74ed33987528f4cf497cb315e50ca5358cea6b52
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-24 20:28:13 +0200

    forgot ;

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -44,6 +44,6 @@
 
   # Electron and Chromium
   ## As of NixOS 22.05 ("Quokka"), you can enable Ozone Wayland support in Chromium and Electron based applications by setting the environment variable NIXOS_OZONE_WL=1. For example, in a configuration.nix: 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1"
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   
 }
