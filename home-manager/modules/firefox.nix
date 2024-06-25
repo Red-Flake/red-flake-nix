370a5c36fac9b6690da0fae27669d9fbcc7a4fc9
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-26 00:03:25 +0200

    firefox enable smooth scroll on wayland via apz.gtk.kinetic_scroll.enabled

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -152,22 +152,7 @@
         user_pref("ui.systemUsesDarkTheme", 1);
         user_pref("browser.translations.automaticallyPopup", false);
 
-        user_pref("apz.gtk.kinetic_scroll.enabled", false);
-        user_pref("general.smoothScroll.lines.durationMaxMS", 125);
-        user_pref("general.smoothScroll.lines.durationMinMS", 125);
-        user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
-        user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
-        user_pref("general.smoothScroll.msdPhysics.enabled", true);
-        user_pref("general.smoothScroll.other.durationMaxMS", 125);
-        user_pref("general.smoothScroll.other.durationMinMS", 125);
-        user_pref("general.smoothScroll.pages.durationMaxMS", 125);
-        user_pref("general.smoothScroll.pages.durationMinMS", 125);
-        user_pref("mousewheel.min_line_scroll_amount", 30);
-        user_pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
-        user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
-        user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
-        user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
-        user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
+        user_pref("apz.gtk.kinetic_scroll.enabled", true);
 
         user_pref("browser.bookmarks.defaultLocation", "toolbar");
         user_pref("browser.toolbars.bookmarks.visibility", "always");
