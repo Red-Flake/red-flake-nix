701c5e3f33287490f397a04422801f5d273efeec
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-26 21:25:09 +0200

    set kde accent color

--- home-manager/modules/plasma-manager.nix
+++ home-manager/modules/plasma-manager.nix
@@ -149,15 +149,16 @@
     # Some low-level settings:
     #
     configFile = {
-      baloofilerc."Basic Settings"."Indexing-Enabled" = false;
-      kwinrc."org.kde.kdecoration2".ButtonsOnLeft = "SF";
-      kwinrc.Desktops.Number = {
+      "kdeglobals"."General"."AccentColor" = "160,31,52";
+      "baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
+      "kwinrc"."org.kde.kdecoration2"."ButtonsOnLeft" = "SF";
+      "kwinrc"."Desktops"."Number" = {
         value = 8;
         # Forces kde to not change this value (even through the settings app).
         immutable = true;
       };
-      kscreenlockerrc = {
-        Greeter.WallpaperPlugin = "org.kde.potd";
+      "kscreenlockerrc" = {
+        "Greeter"."WallpaperPlugin" = "org.kde.potd";
         # To use nested groups use / as a separator. In the below example,
         # Provider will be added to [Greeter][Wallpaper][org.kde.potd][General].
         "Greeter/Wallpaper/org.kde.potd/General".Provider = "bing";
