a80c6af4a8505de47f1d8a344175a4b46db85ef9
Mag1cByt3s <ppeinecke@protonmail.com>
2024-06-26 22:02:00 +0200

    remove deprecated hardware.opengl.driSupport

--- nixos/modules/gpu.nix
+++ nixos/modules/gpu.nix
@@ -9,10 +9,6 @@
   hardware.opengl = {
     enable = true;
 
-    ## Vulkan
-    driSupport = true;
-    driSupport32Bit = true;
-
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
