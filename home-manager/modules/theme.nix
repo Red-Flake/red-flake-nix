78e9107c30a2098f4601d6f29652de72374a103f
Mag1cByt3s <ppeinecke@protonmail.com>
2024-07-02 17:26:56 +0200

    gtk cursor size fixed except for bloodhound

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -41,10 +41,10 @@
 
   # Generic fix for gtk cursor size
   # https://www.reddit.com/r/NixOS/comments/18qpj78/comment/kf0oyjw/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-  home.file.".local/share/icons/default/index.theme".text = ''
-    [Icon Theme]
-    Inherits=breeze_cursors
-  '';
+  #home.file.".local/share/icons/default/index.theme".text = ''
+  #  [Icon Theme]
+  #  Inherits=breeze_cursors
+  #'';
 
   # Fix GTK themes not applied in Wayland
   dconf = {
@@ -58,6 +58,12 @@
   };
 
   # Set X11 cursor theme
+  home.pointerCursor = {
+    name = "Breeze";
+    gtk.enable = true;
+    package = pkgs.kdePackages.breeze;
+    size = 24;
+  };
   #home.pointerCursor = {
   #  gtk.enable = true;
   #  package = pkgs.kdePackages.breeze;
