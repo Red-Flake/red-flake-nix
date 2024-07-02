f12a489e0d1239a55d96c242cda499aac5fe5537
Mag1cByt3s <ppeinecke@protonmail.com>
2024-07-02 21:04:57 +0200

    set custom /etc/os-release

--- nixos/modules/theming.nix
+++ nixos/modules/theming.nix
@@ -1,18 +1,41 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-    # set /etc/issue
     environment.etc = {
-        issue.text = ''
-        Welcome to
 
-        ██████  ███████ ██████      ███████ ██       █████  ██   ██ ███████ 
-        ██   ██ ██      ██   ██     ██      ██      ██   ██ ██  ██  ██      
-        ██████  █████   ██   ██     █████   ██      ███████ █████   █████   
-        ██   ██ ██      ██   ██     ██      ██      ██   ██ ██  ██  ██      
-        ██   ██ ███████ ██████      ██      ███████ ██   ██ ██   ██ ███████ 
-                                                                    
+         # set /etc/issue
+        issue.text = ''
+            Welcome to
+
+            ██████  ███████ ██████      ███████ ██       █████  ██   ██ ███████ 
+            ██   ██ ██      ██   ██     ██      ██      ██   ██ ██  ██  ██      
+            ██████  █████   ██   ██     █████   ██      ███████ █████   █████   
+            ██   ██ ██      ██   ██     ██      ██      ██   ██ ██  ██  ██      
+            ██   ██ ███████ ██████      ██      ███████ ██   ██ ██   ██ ███████ 
+
+        '';
+
+        # set /etc/os-release
+        os-release.text = ''
+            ANSI_COLOR="1;34"
+            BUG_REPORT_URL="https://github.com/Red-Flake/red-flake-nix/issues"
+            BUILD_ID="rolling"
+            DOCUMENTATION_URL="https://github.com/Red-Flake/red-flake-nix/wiki"
+            HOME_URL="https://github.com/Red-Flake/"
+            ID=redflake
+            IMAGE_ID="rolling"
+            IMAGE_VERSION="rolling"
+            LOGO="nix-snowflake"
+            NAME="Red Flake"
+            PRETTY_NAME="Red Flake"
+            SUPPORT_URL="https://github.com/Red-Flake/"
+            VERSION="rolling"
+            VERSION_CODENAME=rolling
+            VERSION_ID="rolling"
         '';
     };
+
+    
+
 
 }
