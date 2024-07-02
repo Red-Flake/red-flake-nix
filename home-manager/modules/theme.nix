9e1eefea148090c2a5a69f065723fb35142bb0e6
Mag1cByt3s <ppeinecke@protonmail.com>
2024-07-02 17:07:16 +0200

    set gtk icon theme to Papirus-Dark

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -8,8 +8,8 @@
       package = pkgs.kdePackages.breeze-gtk;
     };
     iconTheme = {
-      name = "Flat-Remix-Blue-Dark";
-      package = pkgs.flat-remix-icon-theme;
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
     cursorTheme = {
       name = "Breeze";
@@ -29,8 +29,8 @@
 
   # force creation of .gtkrc-2.0 otherwise home-manager will fail
   home.file.${config.gtk.gtk2.configLocation}.force = true;
-  home.file.${config.gtk.gtk3.configLocation}.force = true;
-  home.file.${config.gtk.gtk4.configLocation}.force = true;
+  #home.file.${config.gtk.gtk3.configLocation}.force = true;
+  #home.file.${config.gtk.gtk4.configLocation}.force = true;
 
   # Enabling this seems to break the KDE settings app ...
   #qt = {
