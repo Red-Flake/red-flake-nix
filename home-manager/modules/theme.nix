5086dbd9c79178ba06673e56c6551b3345461f8e
Mag1cByt3s <ppeinecke@protonmail.com>
2024-07-03 07:13:37 +0200

    change cursor theme to Bibata_Cursor in order to increase uniformity between GTK and QT

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -12,9 +12,9 @@
       package = pkgs.papirus-icon-theme;
     };
     cursorTheme = {
-      name = "breeze_cursors";
-      package = pkgs.kdePackages.breeze;
-      size = 24;
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      # size = 24;
     };
     gtk2 = {
       configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
@@ -54,19 +54,19 @@
         gtk-theme = lib.mkForce "Breeze-Dark";
         icon-theme = lib.mkForce "Papirus-Dark";
         color-scheme = lib.mkForce "prefer-dark";
-        cursor-theme = lib.mkForce "breeze_cursors";
-        cursor-size = lib.mkForce 24;
+        cursor-theme = lib.mkForce "Bibata-Modern-Classic";
+        #cursor-size = lib.mkForce 26;
       };
     };
   };
 
   # Set X11 cursor theme
   home.pointerCursor = {
-    name = "breeze_cursors";
+    name = "Bibata-Modern-Classic";
     gtk.enable = true;
     x11.enable = true;
-    package = pkgs.kdePackages.breeze;
-    size = 24;
+    package = pkgs.bibata-cursors;
+    #size = 26;
   };
   #home.pointerCursor = {
   #  gtk.enable = true;
