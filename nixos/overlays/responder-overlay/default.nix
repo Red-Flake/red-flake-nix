cfd26eb54ffb7049f682549fb12a4b9d5c3ae0d5
Mag1cByt3s <ppeinecke@protonmail.com>
2024-07-23 06:44:42 +0200

    fix issue with responder resetting Responder.conf on launch

--- nixos/overlays/responder-overlay/default.nix
+++ nixos/overlays/responder-overlay/default.nix
@@ -8,20 +8,20 @@ final: prev:
     installPhase = ''
       runHook preInstall
 
-      mkdir -p $out/bin $out/share $out/share/Responder
+      mkdir -p $out/bin $out/share/Responder
       cp -R . $out/share/Responder
 
       makeWrapper ${prev.python3.interpreter} $out/bin/responder \
         --set PYTHONPATH "$PYTHONPATH:$out/share/Responder" \
         --add-flags "$out/share/Responder/Responder.py" \
-        --run "mkdir -p /tmp/Responder"
+        --run "mkdir -p /var/lib/responder"
 
       substituteInPlace $out/share/Responder/Responder.conf \
-        --replace "Responder-Session.log" "/tmp/Responder/Responder-Session.log" \
-        --replace "Poisoners-Session.log" "/tmp/Responder/Poisoners-Session.log" \
-        --replace "Analyzer-Session.log" "/tmp/Responder/Analyzer-Session.log" \
-        --replace "Config-Responder.log" "/tmp/Responder/Config-Responder.log" \
-        --replace "Responder.db" "/tmp/Responder/Responder.db"
+        --replace-quiet "Responder-Session.log" "/var/lib/responder/Responder-Session.log" \
+        --replace-quiet "Poisoners-Session.log" "/var/lib/responder/Poisoners-Session.log" \
+        --replace-quiet "Analyzer-Session.log" "/var/lib/responder/Analyzer-Session.log" \
+        --replace-quiet "Config-Responder.log" "/var/lib/responder/Config-Responder.log" \
+        --replace-quiet "Responder.db" "/var/lib/responder/Responder.db"
 
       runHook postInstall
 
@@ -30,30 +30,32 @@ final: prev:
 
     postInstall = ''
       wrapProgram $out/bin/responder \
-        --run "mkdir -p /tmp/Responder/certs && ${prev.openssl}/bin/openssl genrsa -out /tmp/Responder/certs/responder.key 2048 && ${prev.openssl}/bin/openssl req -new -x509 -days 3650 -key /tmp/Responder/certs/responder.key -out /tmp/Responder/certs/responder.crt -subj '/'"
+        --run "mkdir -p /var/lib/responder/certs && ${prev.openssl}/bin/openssl genrsa -out /var/lib/responder/certs/responder.key 2048 && ${prev.openssl}/bin/openssl req -new -x509 -days 3650 -key /var/lib/responder/certs/responder.key -out /var/lib/responder/certs/responder.crt -subj '/'" \
+        --run "mkdir -p /etc/responder && if [ ! -f /etc/responder/Responder.conf ]; then cp $out/share/Responder/Responder.conf /etc/responder/Responder.conf && chmod +w /etc/responder/Responder.conf; fi"
     '';
 
     postPatch = ''
       if [ -f $out/share/Responder/settings.py ]; then
         substituteInPlace $out/share/Responder/settings.py \
-          --replace "self.LogDir = os.path.join(self.ResponderPATH, 'logs')" "self.LogDir = os.path.join('/tmp/Responder/', 'logs')"
+          --replace-quiet "self.LogDir = os.path.join(self.ResponderPATH, 'logs')" "self.LogDir = os.path.join('/var/lib/responder', 'logs')" \
+          --replace-quiet "os.path.join(self.ResponderPATH, 'Responder.conf')" "'/etc/responder/Responder.conf'"
       fi
 
       if [ -f $out/share/Responder/utils.py ]; then
         substituteInPlace $out/share/Responder/utils.py \
-          --replace "logfile = os.path.join(settings.Config.ResponderPATH, 'logs', fname)" "logfile = os.path.join('/tmp/Responder/', 'logs', fname)"
+          --replace-quiet "logfile = os.path.join(settings.Config.ResponderPATH, 'logs', fname)" "logfile = os.path.join('/var/lib/responder', 'logs', fname)"
       fi
 
       if [ -f $out/share/Responder/Responder.py ]; then
         substituteInPlace $out/share/Responder/Responder.py \
-          --replace "certs/responder.crt" "/tmp/Responder/certs/responder.crt" \
-          --replace "certs/responder.key" "/tmp/Responder/certs/responder.key"
+          --replace-quiet "certs/responder.crt" "/var/lib/responder/certs/responder.crt" \
+          --replace-quiet "certs/responder.key" "/var/lib/responder/certs/responder.key"
       fi
 
       if [ -f $out/share/Responder/Responder.conf ]; then
         substituteInPlace $out/share/Responder/Responder.conf \
-          --replace "certs/responder.crt" "/tmp/Responder/certs/responder.crt" \
-          --replace "certs/responder.key" "/tmp/Responder/certs/responder.key"
+          --replace-quiet "certs/responder.crt" "/var/lib/responder/certs/responder.crt" \
+          --replace-quiet "certs/responder.key" "/var/lib/responder/certs/responder.key"
       fi
     '';
   });
