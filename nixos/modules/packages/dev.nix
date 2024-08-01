db71662a1b4fb9b766ed61277edb5a94de11faa2
Mag1cByt3s <ppeinecke@protonmail.com>
2024-08-01 17:10:14 +0200

    group packages into categories

---
+++ nixos/modules/packages/dev.nix
@@ -0,0 +1,14 @@
+{ inputs, config, lib, pkgs, modulesPath, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    gnumake
+    cmake
+    dotnet-sdk_8
+    dotnet-runtime_8
+    dotnet-aspnetcore_8
+    jdk
+  ];
+}
---
+++ nixos/modules/packages/general.nix
@@ -0,0 +1,54 @@
+{ inputs, config, lib, pkgs, modulesPath, ... }:
+
+{
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    file
+    gparted
+    tree
+    wget
+    unzip
+    unrar
+    htop
+    btop
+    fastfetch
+    lsb-release
+    socat
+    rlwrap
+    thefuck
+    dfc
+    zsh
+    openvpn
+    macchanger
+    soapui
+    ghex
+    jq
+    ntp
+    redis
+    imagemagick
+    strace
+    # clamav broken at the moment
+    #clamav
+    # xsser broken at the moment due to python nose
+    #xsser
+    lsd
+    bat
+    man
+    less
+    grc
+    libvncserver
+    lshw
+    lshw-gui
+    thunderbird-bin
+    helvum
+    mission-center
+    psmisc
+    aha
+    pciutils
+    clinfo
+    glxinfo
+    vulkan-tools
+    wayland-utils
+  ];
+}
