8112e2c20c2b95447eb2315fdbe7112f6c38ddfa
Mag1cByt3s <ppeinecke@protonmail.com>
2024-08-06 16:03:42 +0200

    try to fix disko

--- flake.nix
+++ flake.nix
@@ -106,6 +106,7 @@
 
         modules = [
           disko.nixosModules.disko
+          ./disko.nix
           chaotic.nixosModules.default
           nixos-boot.nixosModules.default
           darkmatter-grub-theme.nixosModule
