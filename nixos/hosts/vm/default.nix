19e090c16a6fe004ff55dc3d72618c478df3e7e7
Mag1cByt3s <ppeinecke@protonmail.com>
2024-08-11 18:41:59 +0200

    seperate nix & nixpkgs config

--- nixos/hosts/vm/default.nix
+++ nixos/hosts/vm/default.nix
@@ -4,6 +4,12 @@
 {
   # Import other NixOS modules here
   imports = [
+    # Nix configuration
+    ../../modules/nix.nix
+
+    # Nixpkgs configuration
+    ../../modules/nixpkgs.nix
+
     # Additional hardware configuration
     ../../modules/hardware.nix
 
@@ -74,130 +80,6 @@
     ../../modules/tweaks.nix
 
   ];
-
-  nixpkgs = {
-
-    # Set host platform
-    hostPlatform = lib.mkDefault "x86_64-linux";
-
-    # You can add overlays here
-    overlays = with inputs; [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Chaotic Nyx overlay
-      chaotic.overlays.default
-
-      # NUR overlay
-      nur.overlay
-
-      # impacket overlay
-      (import ./overlays/impacket-overlay)
-
-      # responder overlay
-      (import ./overlays/responder-overlay)
-
-      # evil-winrm overlay
-      (import ./overlays/evil-winrm-overlay)
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-
-      # temporary FIX for https://github.com/NixOS/nixpkgs/issues/325657  /  https://github.com/NixOS/nixpkgs/pull/325676
-      (_: prev: {
-        #python312 = prev.python312.override { packageOverrides = _: pysuper: { nose = pysuper.pynose; }; };
-        pwndbg = prev.python311Packages.pwndbg;
-        pwntools = prev.python311Packages.pwntools;
-        ropper = prev.python311Packages.ropper;
-        gef = prev.gef.override { python3 = prev.python311; };
-        compiler-rt = prev.compiler-rt.override { python3 = prev.python311; };
-        wordlists = prev.wordlists.override { wfuzz = prev.python311Packages.wfuzz; };
-        thefuck = prev.thefuck.overridePythonAttrs { doCheck = false; };
-        #xsser = prev.xsser.override { python3 = prev.python311; };
-        ananicy-cpp = prev.ananicy-cpp.overrideAttrs { hardeningDisable = [ "zerocallusedregs" ]; };
-      })
-
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-
-      # Allow legacy packages
-      permittedInsecurePackages = [
-        "openssl-1.1.1w"
-      ];
-    };
-  };
-
-  nix = let
-    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  in {
-    optimise.automatic = true;
-
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-
-    # Don't warn about dirty flakes and accept flake configs by default
-    extraOptions = ''
-      accept-flake-config = true
-      warn-dirty = false
-    '';
-
-    settings = {
-      # Use available binary caches, this is not Gentoo
-      # this also allows us to use remote builders to reduce build times and batter usage
-      builders-use-substitutes = true;
-
-      # Enable flakes and new 'nix' command
-      experimental-features = [ "nix-command" "flakes" ];
-
-      # Opinionated: disable global registry
-      flake-registry = "";
-
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
-
-      # Enable deduplication
-      auto-optimise-store = true;
-
-      # Substituters
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org/"
-      ];
-
-      # Trusted public keys
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-
-      # A few extra binary caches and their public keys
-      # Enable Cachix Binary Cache for Chaotic-Nyx
-      extra-substituters = [ "https://chaotic-nyx.cachix.org" ];
-      extra-trusted-public-keys = [ "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=" ];
-
-      # Show more log lines for failed builds
-      log-lines = 20;
-
-      # Max number of parallel jobs
-      max-jobs = "auto";
-    };
-
-    # Opinionated: disable channels
-    channel.enable = false;
-
-    # Opinionated: make flake registry and nix path match flake inputs
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
 
   # Set hostname
   networking.hostName = "redflake-vm";
