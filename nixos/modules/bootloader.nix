526ecf35a0af13742f5d7443aafab1411eba7151
Mag1cByt3s <ppeinecke@protonmail.com>
2024-08-12 22:01:34 +0200

    set devNodes to /dev/disk/by-label

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -78,12 +78,12 @@ in
   # ZFS settings
   boot.zfs = {
   # use by-id for intel mobo when not in a vm
-    devNodes =
-      if !isVm then
-        "/dev/disk/by-partlabel"
-      else
-        "/dev/disk/by-label";
-      #devNodes = "/dev/";
+    #devNodes =
+    #  if !isVm then
+    #    "/dev/disk/by-partlabel"
+    #  else
+    #    "/dev/disk/by-label";
+    devNodes = "/dev/disk/by-label";
     package = pkgs.zfs_unstable;
     requestEncryptionCredentials = cfg.encryption;
   };
