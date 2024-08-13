af8f636fc4dc91a6280c0427d0a1bf223f27918a
Mag1cByt3s <ppeinecke@protonmail.com>
2024-08-13 05:50:12 +0200

    add flake helpers for host-based and user-based configuration

---
+++ lib/helpers.nix
@@ -0,0 +1,69 @@
+inputs:
+
+let
+  lib = inputs.nixpkgs.lib;
+in
+rec {
+
+  mkHost =
+    { host
+    , user
+    , system ? "x86_64-linux"
+    , nixosModules ? [ ]
+    , homeModules ? [ ]
+    , nixpkgs ? inputs.unstable
+    }:
+    let pkgs = mkNixpkgs { inherit nixpkgs system; };
+    in
+    nixpkgs.lib.nixosSystem {
+      inherit system;
+
+      specialArgs = {
+        inherit pkgs inputs system nixpkgs;
+        inherit (inputs) homeManager nur hardware;
+      };
+
+      modules =
+        [
+          # host configuration
+          (../nixos/hosts + "/${host}")
+
+          # home manager
+          inputs.homeManager.nixosModules.home-manager
+          {
+            home-manager = {
+              useUserPackages = true;
+              users."${user}" = {
+                imports = homeModules;
+              };
+              extraSpecialArgs = {
+                inherit inputs pkgs nixpkgs user;
+              };
+            };
+          }
+        ];
+    };
+
+
+  mkHome =
+    { user
+    , system
+    , homeModules ? [ ]
+    , nixpkgs ? inputs.unstable
+    }:
+    let
+      pkgs = mkNixpkgs { inherit system; };
+      homeDirectory = "/home/${user}";
+    in
+    inputs.homeManager.lib.homeManagerConfiguration {
+      inherit system user homeDirectory pkgs;
+      configuration = {
+        imports = homeModules;
+      };
+
+      extraSpecialArgs = {
+        inherit inputs nixpkgs system user homeDirectory;
+      };
+    };
+
+}
