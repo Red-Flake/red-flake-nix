00d08951f70b1c9fbcbe10c8d7645e40881fd37b
Mag1cByt3s <ppeinecke@protonmail.com>
2024-09-10 07:47:11 +0200

    add neccesary files and directories to environment.persistence

--- nixos/modules/impermanence.nix
+++ nixos/modules/impermanence.nix
@@ -60,12 +60,20 @@ in
                "/etc/machine-id" # for persistent machine-id
              ] ++ [
                # YOUR FILES
+               "/etc/hostname" # for persistent hostname
+               "/etc/hosts" # for persistent hosts
              ];
              directories = [
                "/var/log" # systemd journal is stored in /var/log/journal
                "/var/lib/nixos" # for persisting user uids and gids
              ] ++ [
                # YOUR DIRECTORIES
+               "/etc/NetworkManager" # persist network manager configuration
+               "/var/lib/NetworkManager" # persist network manager data
+               "/var/lib/neo4j" # persist neo4j data
+               "/var/lib/postgres" # persist postgres data
+               "/var/lib/docker" # persist docker data
+               "/var/lib/flatpak" # persist flatpak data
              ];
          };
 
