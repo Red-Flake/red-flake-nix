238057fae606ea726983a743d924ee456fc363e5
Mag1cByt3s <ppeinecke@protonmail.com>
2024-09-10 04:53:57 +0200

    fix config.custom.persist

--- nixos/modules/impermanence.nix
+++ nixos/modules/impermanence.nix
@@ -1,29 +1,79 @@
 { 
+  config,
   lib,
   pkgs,
   modulesPath,
   ... 
-}: {
-   # setup persistence
-   environment.persistence = {
-      "/persist" = {
-          hideMounts = true;
-          files = [ 
-            "/etc/machine-id" # for persistent machine-id
-          ] ++ [
-            # YOUR FILES
-          ];
-          directories = [
-            "/var/log" # systemd journal is stored in /var/log/journal
-            "/var/lib/nixos" # for persisting user uids and gids
-          ] ++ [
-            # YOUR DIRECTORIES
-          ];
+}: 
+let
+  cfg = config.custom.persist;
+in
+{
+    options.custom = with lib; {
+      persist = {
+        root = {
+          directories = mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+            description = "Directories to persist in root filesystem";
+          };
+          files = mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+            description = "Files to persist in root filesystem";
+          };
+          cache = {
+            directories = mkOption {
+              type = types.listOf types.str;
+              default = [ ];
+              description = "Directories to persist, but not to snapshot";
+            };
+            files = mkOption {
+              type = types.listOf types.str;
+              default = [ ];
+              description = "Files to persist, but not to snapshot";
+            };
+          };
+        };
+        home = {
+          directories = mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+            description = "Directories to persist in home directory";
+          };
+          files = mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+            description = "Files to persist in home directory";
+          };
+        };
       };
+    };
 
-      "/persist/cache" = {
-          hideMounts = true;
-          inherit (cfg.root.cache) directories files;
+    config = {
+
+      # setup persistence
+      environment.persistence = {
+         "/persist" = {
+             hideMounts = true;
+             files = [ 
+               "/etc/machine-id" # for persistent machine-id
+             ] ++ [
+               # YOUR FILES
+             ];
+             directories = [
+               "/var/log" # systemd journal is stored in /var/log/journal
+               "/var/lib/nixos" # for persisting user uids and gids
+             ] ++ [
+               # YOUR DIRECTORIES
+             ];
+         };
+
+         "/persist/cache" = {
+             hideMounts = true;
+             inherit (cfg.root.cache) directories files;
+         };
       };
-   };
+    
+    };
 }
