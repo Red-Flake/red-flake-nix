8ab44aeca56936a93791752f4465211c4748ab87
Mag1cByt3s <ppeinecke@protonmail.com>
2024-09-12 22:41:39 +0200

    try to fix issues with persistence

--- nixos/modules/impermanence.nix
+++ nixos/modules/impermanence.nix
@@ -82,17 +82,17 @@ in
              ];
          };
       };
+
+      # setup persistence for NetworkManager, Docker, LXC
+      environment.etc."NetworkManager/system-connections".source = "/persist/etc/NetworkManager/system-connections";
+      systemd.tmpfiles.rules = [
+        "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
+        "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
+        "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
+        "L /var/lib/lxd - - - - /persist/var/lib/lxd"
+        "L /var/lib/docker - - - - /persist/var/lib/docker"
+      ];
     
     };
-
-    # setup persistence for NetworkManager, Docker, LXC
-    environment.etc."NetworkManager/system-connections".source = "/persist/etc/NetworkManager/system-connections";
-    systemd.tmpfiles.rules = [
-      "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
-      "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
-      "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
-      "L /var/lib/lxd - - - - /persist/var/lib/lxd"
-      "L /var/lib/docker - - - - /persist/var/lib/docker"
-    ];
 
 }
