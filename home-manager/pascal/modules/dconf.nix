331ecf45269ff49334de8d63b5f40ecc9e9ed087
Mag1cByt3s <ppeinecke@protonmail.com>
2024-12-23 09:10:26 +0100

    set X11 cursor theme to Sweet-cursors via dconf

--- home-manager/pascal/modules/dconf.nix
+++ home-manager/pascal/modules/dconf.nix
@@ -1,21 +1,15 @@
 { config, lib, pkgs, ... }:
 {
     # dconf user settings
-    # Fix GTK themes not applied in Wayland
-    dconf.settings = {
-        
-        "org/gnome/desktop/interface" = {
-          gtk-theme = lib.mkForce "Breeze-Dark";
-          icon-theme = lib.mkForce "Papirus-Dark";
-          color-scheme = lib.mkForce "prefer-dark";
-          cursor-theme = lib.mkForce "Bibata-Modern-Classic";
-          cursor-size = lib.mkForce 24;
-        };
+  dconf = {
+    enable = true;
+    settings = {
 
         "org/virt-manager/virt-manager/connections" = {
           autoconnect = ["qemu:///system"];
           uris = ["qemu:///system"];
         };
-
+    
+    };
   };
 }
--- home-manager/pascal/modules/theme.nix
+++ home-manager/pascal/modules/theme.nix
@@ -39,20 +39,21 @@
   # force creation of ~/.config/gtk-4.0/settings.ini otherwise home-manager will fail
   xdg.configFile."gtk-4.0/settings.ini".force = true;
 
-  # dconf settings broken atm
   # Fix GTK themes not applied in Wayland
-  #dconf = {
-  #  enable = true;
-  #  settings = {
-  #    "org/gnome/desktop/interface" = {
-  #      gtk-theme = lib.mkForce "Breeze-Dark";
-  #      icon-theme = lib.mkForce "Papirus-Dark";
-  #      color-scheme = lib.mkForce "prefer-dark";
-  #      cursor-theme = lib.mkForce "Bibata-Modern-Classic";
-  #      cursor-size = lib.mkForce 24;
-  #    };
-  #  };
-  #};
+  dconf = {
+    enable = true;
+    settings = {
+
+      "org/gnome/desktop/interface" = {
+        gtk-theme = lib.mkForce "Breeze-Dark";
+        icon-theme = lib.mkForce "Papirus-Dark";
+        color-scheme = lib.mkForce "prefer-dark";
+        cursor-theme = lib.mkForce "Sweet-cursors";
+        cursor-size = lib.mkForce 24;
+      };
+      
+    };
+  };
 
   # Set X11 cursor theme
   home.pointerCursor = {
