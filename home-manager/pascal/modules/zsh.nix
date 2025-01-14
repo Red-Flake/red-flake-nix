4dd494c35190590e610e179915edca3392704dd8
Mag1cByt3s <ppeinecke@protonmail.com>
2025-01-15 00:14:04 +0100

    add zsh alias for mono-csc

--- home-manager/pascal/modules/zsh.nix
+++ home-manager/pascal/modules/zsh.nix
@@ -110,7 +110,8 @@ in
                     uploadserver = "pipx run uploadserver";
                     bloodhound-quickwin = "bhqc.py";
                     stegsolve = "java -jar /usr/share/tools/Steganography/stegsolve.jar";
-		    msfconsole = "msfconsole -q";
+		                msfconsole = "msfconsole -q";
+                    mono-csc = "csc";
 
                     # nixos
                     redflake-rebuild = "bash <(curl -L https://raw.githubusercontent.com/Red-Flake/red-flake-nix/main/rebuild.sh)";
