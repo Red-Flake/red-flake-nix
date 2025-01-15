6a06cd9316c825df4c6283a3c38bcfe500126168
Mag1cByt3s <ppeinecke@protonmail.com>
2025-01-15 01:45:52 +0100

    add environment.pathsToLink to xdg.nix

--- nixos/modules/xdg.nix
+++ nixos/modules/xdg.nix
@@ -19,4 +19,7 @@
 
   # enable XDG terminal exec
   xdg.terminal-exec.enable = true;
+
+  # if you use the NixOS module and have useUserPackages = true, make sure to add:
+  environment.pathsToLink = [ "/share/xdg-desktop-portal" "/share/applications" ];
 }
