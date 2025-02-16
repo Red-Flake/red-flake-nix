b24a27d778af115606ae17e283cc3a392823bcbe
Mag1cByt3s <ppeinecke@protonmail.com>
2025-02-16 07:01:21 +0100

    rebase netexec overlay to latest version

--- nixos/overlays/netexec-overlay/default.nix
+++ nixos/overlays/netexec-overlay/default.nix
@@ -6,12 +6,12 @@ final: prev:
     self = final.python3;
     packageOverrides = self: super: {
       impacket = super.impacket.overridePythonAttrs (old: {
-        version = "16.12.2024"; # Updated version
+        version = "0.12.0-unstable-06.02.2025"; # Updated version
         src = final.fetchFromGitHub {
           owner = "fortra"; # Replace with the official or updated repository owner
           repo = "impacket";
-          rev = "67e19240fa66ee9f461b506d7f50723e5960cb7c"; # Replace with the commit hash of the newer version
-          hash = "sha256-ueSJ7/kTAR/OqL5k1bebOOY3vHfLpdWT0zXfDJ5XZDc="; # Replace with the SHA-256 hash of the new source; nix-prefetch-url --unpack "https://github.com/fortra/impacket/archive/db53482dc864fec69156898d52c1b595a777ca9a.tar.gz"
+          rev = "075f2b10a7a4056374a8c917f75e4419817cd6c7"; # Replace with the commit hash of the newer version
+          hash = "sha256-PjpactVeJLnTLxwxjbskpmkeBeAjihqqkhhEivl6xH4="; # Replace with the SHA-256 hash of the new source; nix-prefetch-url --unpack "https://github.com/fortra/impacket/archive/db53482dc864fec69156898d52c1b595a777ca9a.tar.gz"
         };
 
         # Fix version to be compliant with Python packaging rules
@@ -45,7 +45,7 @@ final: prev:
 
   netexec = final.python3.pkgs.buildPythonApplication rec {
     pname = "netexec";
-    version = "21.12.2024"; # Updated version
+    version = "12.02.2025"; # Updated version
     pyproject = true;
     pythonRelaxDeps = true;
     pythonRemoveDeps = [
@@ -56,8 +56,8 @@ final: prev:
     src = final.fetchFromGitHub {
       owner = "Pennyw0rth";
       repo = "NetExec";
-      rev = "5103f57face6e7c522424c02d4151123eb482f04"; # Replace with the updated revision if needed
-      hash = "sha256-WR9JBuGp+IZHitHYV6thn67Me/8BKjrNXzTVwna2+xI="; # Replace with the SHA-256 hash of the new source; nix-prefetch-url --unpack "https://github.com/Pennyw0rth/NetExec/archive/6d4fdfdb2d0088405ea3139f4145f198671a0fda.tar.gz"
+      rev = "c2f85db63b5b4323ac6f993eefa553ad276fe12f"; # Replace with the updated revision if needed
+      hash = "sha256-FMroXBqpufruqCY4iebOTPuqy2v9ZEN+bOhtWaxLqn0="; # Replace with the SHA-256 hash of the new source; nix-prefetch-url --unpack "https://github.com/Pennyw0rth/NetExec/archive/6d4fdfdb2d0088405ea3139f4145f198671a0fda.tar.gz"
     };
 
     postPatch = ''
