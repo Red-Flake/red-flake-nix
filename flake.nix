38659102f74eabfe0e7b0990bfdc9f894f42e6a5
Mag1cByt3s <ppeinecke@protonmail.com>
2025-03-13 20:24:04 +0100

    disable pubkey for nyx.chaotic.cx for now due to issues. Update tools input

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       ];
       extra-trusted-public-keys = [
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        #"nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
   };
