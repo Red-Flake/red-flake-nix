8f385d453b405d513454a67e1582b43063abaae7
Mag1cByt3s <ppeinecke@protonmail.com>
2025-04-02 21:44:14 +0200

    rebased wordlists overlay to latest commit hash

--- nixos/overlays/wordlists-overlay/default.nix
+++ nixos/overlays/wordlists-overlay/default.nix
@@ -6,8 +6,8 @@ self: super: {
     # Fetch the repository using fetchgit with Git LFS support.
     src = super.fetchgit {
       url = "https://github.com/Red-Flake/wordlists.git";
-      rev = "4087cc5e82f4c69c717cf51435d1294b5f63df8c"; # e.g. commit hash or branch name
-      sha256 = "sha256-FtWlNcUJNVEEu97hiWM0p9YDQ9HQZh69SoBDa1vgGnM=";
+      rev = "2c314dee7891da8b29f313ddc1c72023b3527b89"; # e.g. commit hash or branch name
+      sha256 = "sha256-hzoIYxb/qJqc3piOhXdlUT9e+U5zU1OD0zZ9W91HFDQ=";
       fetchLFS = true;
     };
 
