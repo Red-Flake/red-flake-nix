24f5caf37e2eada698507c923832246251915bd2
Mag1cByt3s <ppeinecke@protonmail.com>
2025-04-21 05:49:49 +0200

    apply patch fix-pyasn1-bool.patch to netexec overlay

--- nixos/overlays/netexec-overlay/default.nix
+++ nixos/overlays/netexec-overlay/default.nix
@@ -6,13 +6,17 @@ final: prev:
     self = final.python312;
     packageOverrides = self: super: {
       impacket = super.impacket.overridePythonAttrs (old: {
-        version = "0.12.0-unstable-16.04.2025"; # Updated version
+        version = "0.12.0-unstable-04.04.2025"; # Updated version
         src = final.fetchFromGitHub {
           owner = "fortra"; # Replace with the official or updated repository owner
           repo = "impacket";
-          rev = "c384b5fb112febce1d03c93fb32f47df042e72d8"; # Replace with the commit hash of the newer version
+          rev = "00ced47f3ed61b64441146e24042f865dcc017b9"; # Replace with the commit hash of the newer version
           hash = "sha256-Onw2JQ35gTIrl1vsu/c/9GXSbElmLuAEPiwDwScOurk="; # Replace with the SHA-256 hash of the new source; nix-prefetch-url --unpack "https://github.com/fortra/impacket/archive/db53482dc864fec69156898d52c1b595a777ca9a.tar.gz"
         };
+
+        # apply our fix to avoid __bool__ on ASN.1 schema
+        patches = [ ./fix-pyasn1-bool.patch ];
+        patchStrip = 1;
 
         # Fix version to be compliant with Python packaging rules
         postPatch = ''
