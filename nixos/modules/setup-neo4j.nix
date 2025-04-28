0916a449e72d38810e0f6330ba5c6bc8dfbee622
Mag1cByt3s <ppeinecke@protonmail.com>
2025-04-28 16:10:13 +0200

    improved activation script setup-neo4j

--- nixos/modules/setup-neo4j.nix
+++ nixos/modules/setup-neo4j.nix
@@ -1,19 +1,25 @@
 { pkgs, lib, ... }:
 
+let
+  gdsVersion = "2.12.0";
+  gdsJarName = "neo4j-graph-data-science-${gdsVersion}.jar";
+  gdsDownloadUrl = "https://github.com/neo4j/graph-data-science/releases/download/${gdsVersion}/${gdsJarName}";
+in
 {
   system.activationScripts.setup-neo4j = {
     text = ''
-      if [ ! -d /var/lib/neo4j/plugins ]; then
-        mkdir -p /var/lib/neo4j/plugins
+      plugins_dir="/var/lib/neo4j/plugins"
+
+      if [ ! -d "$plugins_dir" ]; then
+        mkdir -p "$plugins_dir"
       fi
 
-      # Update to GDS 2.12.0 for Neo4j 5.26.x compatibility
-      if [ ! -f /var/lib/neo4j/plugins/neo4j-graph-data-science-2.12.0.jar ]; then
-        ${lib.getExe' pkgs.wget "wget"} -O /var/lib/neo4j/plugins/neo4j-graph-data-science-2.12.0.jar \
-          https://github.com/neo4j/graph-data-science/releases/download/2.12.0/neo4j-graph-data-science-2.12.0.jar
+      if [ ! -f "$plugins_dir/${gdsJarName}" ]; then
+        echo "Downloading Graph Data Science ${gdsVersion} plugin..."
+        ${lib.getExe' pkgs.wget "wget"} -O "$plugins_dir/${gdsJarName}" "${gdsDownloadUrl}"
       fi
 
-      # run script to automatically set the neo4j password to Password1337
+      # Set initial Neo4j password
       NEO4J_CONF=/var/lib/neo4j/conf ${lib.getExe' pkgs.neo4j "neo4j-admin"} dbms set-initial-password Password1337
     '';
   };
