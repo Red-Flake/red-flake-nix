e8530256c544849b01140cb3ed627c40e0254b08
Mag1cByt3s <ppeinecke@protonmail.com>
2025-04-28 16:07:36 +0200

    Update to GDS 2.12.0 for Neo4j 5.26.x compatibility

--- nixos/modules/setup-neo4j.nix
+++ nixos/modules/setup-neo4j.nix
@@ -7,9 +7,10 @@
         mkdir -p /var/lib/neo4j/plugins
       fi
 
-      if [ ! -f /var/lib/neo4j/plugins/neo4j-graph-data-science-2.10.1.jar ]; then
-        ${lib.getExe' pkgs.wget "wget"} -O /var/lib/neo4j/plugins/neo4j-graph-data-science-2.10.1.jar \
-          https://github.com/neo4j/graph-data-science/releases/download/2.10.1/neo4j-graph-data-science-2.10.1.jar
+      # Update to GDS 2.12.0 for Neo4j 5.26.x compatibility
+      if [ ! -f /var/lib/neo4j/plugins/neo4j-graph-data-science-2.12.0.jar ]; then
+        ${lib.getExe' pkgs.wget "wget"} -O /var/lib/neo4j/plugins/neo4j-graph-data-science-2.12.0.jar \
+          https://github.com/neo4j/graph-data-science/releases/download/2.12.0/neo4j-graph-data-science-2.12.0.jar
       fi
 
       # run script to automatically set the neo4j password to Password1337
