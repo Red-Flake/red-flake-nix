46e33ce3ba79444ec169008fc9bced7d06e38f18
Mag1cByt3s <ppeinecke@protonmail.com>
2025-05-14 12:54:30 +0200

    firefox: change default search engine to searxng.world

--- home-manager/pascal/modules/firefox.nix
+++ home-manager/pascal/modules/firefox.nix
@@ -19,8 +19,24 @@
       isDefault = true;
 
       search = {
-        default = "duckduckgo";
+        default = "SearXNG";
+        privateDefault = "SearXNG";
         force = true;
+        order = [
+          "SearXNG"
+        ];
+
+        engines = {
+          "SearXNG" = {
+            urls = [{
+              template = "https://searxng.world/search?q={searchTerms}";
+            }];
+
+            icon = "https://searxng.world/static/themes/simple/img/favicon.svg";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = [ "@sx" ];
+          };
+        };
       };
 
       settings = {
