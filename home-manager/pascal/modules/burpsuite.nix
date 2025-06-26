dc5662633677606a01e98667190a736dadbc4eb8
Mag1cByt3s <ppeinecke@protonmail.com>
2025-06-26 02:51:24 +0200

    configure burp pro via home-manager

--- home-manager/pascal/modules/burpsuite.nix
+++ home-manager/pascal/modules/burpsuite.nix
@@ -6,4 +6,25 @@
       recursive = false;
       force = true;
     };
+
+    # deploy UserConfigPro.json
+    home.file.".BurpSuite/UserConfigPro.json" = {
+      source = ./burpsuite/UserConfigPro.json;
+      recursive = false;
+      force = true;
+    };
+
+    # deploy jruby-complete-10.0.0.1.jar
+    home.file.".BurpSuite/jruby-complete-10.0.0.1.jar" = {
+      source = ./burpsuite/jruby-complete-10.0.0.1.jar;
+      recursive = false;
+      force = true;
+    };
+
+    # deploy jython-standalone-2.7.4.jar
+    home.file.".BurpSuite/jython-standalone-2.7.4.jar" = {
+      source = ./burpsuite/jython-standalone-2.7.4.jar;
+      recursive = false;
+      force = true;
+    };
 }
