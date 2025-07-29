15514abc45ce1ad54140081ff3986678510407ff
Mag1cByt3s <ppeinecke@protonmail.com>
2025-07-29 10:00:42 +0200

    switch to scx_bpfland scheduler and enable low latency

--- nixos/modules/performance.nix
+++ nixos/modules/performance.nix
@@ -2,12 +2,22 @@
 
 {
     powerManagement = {
-        enable = lib.mkForce true;
-        cpuFreqGovernor = lib.mkDefault "performance";
-        powertop.enable = lib.mkForce false;
+      enable = lib.mkForce true;
+      cpuFreqGovernor = lib.mkDefault "performance";
+      powertop.enable = lib.mkForce false;
     };
 
     services.tlp = {
       enable = lib.mkForce false;
+    };
+
+    # Enable scx_bpfland scheduler
+    services.scx = {
+      enable = true;
+      scheduler = "scx_bpfland";
+      # Optional: Set extra options for the scheduler
+      extraArgs = [
+        "--lowlatency"  # Optional: For better responsiveness under load
+      ];
     };
 }
