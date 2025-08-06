ac379e59089bd3b0cb1a832d12ee988b7a3c2398
Mag1cByt3s <ppeinecke@protonmail.com>
2025-08-06 23:16:02 +0200

    move systemd.tmpfiles.rules for intel-compute-runtime into intel-legacy-overlay

--- nixos/hosts/t580/hardware.nix
+++ nixos/hosts/t580/hardware.nix
@@ -110,13 +110,4 @@
 
   };
 
-  # create symlink for intel-opencl/libigdrcl_legacy1.so
-  systemd.tmpfiles.rules =
-    let
-      createLink = src: dest: "L+ ${dest} - - - - ${src}";
-    in
-    [
-      (createLink "${pkgs.intel-compute-runtime-legacy1}/lib/intel-opencl/libigdrcl_legacy1.so" "/usr/lib/x86_64-linux-gnu/intel-opencl/libigdrcl_legacy1.so")
-    ];
-
 }
--- nixos/overlays/intel-legacy-overlay/default.nix
+++ nixos/overlays/intel-legacy-overlay/default.nix
@@ -1,3 +1,13 @@
 self: super: {
+  # replace intel-compute-runtime with intel-compute-runtime-legacy1 for legacy Gen8, Gen9 and Gen11 Intel GPUs
   intel-compute-runtime = super.intel-compute-runtime-legacy1;
+
+  # create symlink for intel-opencl/libigdrcl_legacy1.so
+  super.systemd.tmpfiles.rules =
+    let
+      createLink = src: dest: "L+ ${dest} - - - - ${src}";
+    in
+    [
+      (createLink "${super.pkgs.intel-compute-runtime-legacy1}/lib/intel-opencl/libigdrcl_legacy1.so" "/usr/lib/x86_64-linux-gnu/intel-opencl/libigdrcl_legacy1.so")
+    ];
 }
