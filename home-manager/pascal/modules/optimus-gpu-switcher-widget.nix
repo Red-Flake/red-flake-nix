192ad15a20231c1ee97e39bcf9a82b856ec3db74
Mag1cByt3s <ppeinecke@protonmail.com>
2025-09-03 10:19:48 +0200

    kde: check if optimus-gpu-switcher-widget already exists

--- home-manager/pascal/modules/optimus-gpu-switcher-widget.nix
+++ home-manager/pascal/modules/optimus-gpu-switcher-widget.nix
@@ -11,9 +11,14 @@ let
     rev = "b568fa349bf015fc71cb087a91800c81ae8afc9f";
     sha256 = "sha256-4/k6IhMuXpuBt8H8EgY6o00WE1V0JKliMycPpY6L5wM=";
   };
+  widgetDest = "$HOME/.local/share/plasma/plasmoids/optimus-gpu-switcher";
 in
 {
   home.activation.plasmaWidgets = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.kdePackages.kpackage}/bin/kpackagetool6 -t Plasma/Applet -i ${optimusWidget}
+    if [ ! -d "${widgetDest}" ]; then
+      ${pkgs.kdePackages.kpackage}/bin/kpackagetool6 -t Plasma/Applet -i ${optimusWidget}
+    else
+      echo "Plasmoid already installed, skipping..."
+    fi
   '';
 }
