cdc1477fccfcc09254d5b8322edd779b57a1a7d3
Mag1cByt3s <ppeinecke@protonmail.com>
2025-09-10 00:14:34 +0200

    force cpu governor performance

--- nixos/modules/performance.nix
+++ nixos/modules/performance.nix
@@ -1,24 +1,32 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-    powerManagement = {
-      enable = lib.mkForce true;
-      cpuFreqGovernor = lib.mkDefault "performance";
-      powertop.enable = lib.mkForce false;
-    };
+  powerManagement = {
+    enable = lib.mkForce true;
+    cpuFreqGovernor = lib.mkForce "performance";
+    powertop.enable = lib.mkForce false;
+  };
 
-    services.tlp = {
-      enable = lib.mkForce false;
-    };
+  services.tlp = {
+    enable = lib.mkForce false;
+  };
 
-    # Enable scx_bpfland scheduler
-    services.scx = {
-      enable = true;
-      scheduler = "scx_bpfland";
-      # Optional: Set extra options for the scheduler
-      extraArgs = [
-        "--slice-us-lag" "-5000"  # Negative lag for more consistent performance (reduces responsiveness but smooths long-running tasks)
-        "--primary-domain" "performance"  # Prioritize fastest cores initially (e.g., for bursty compilation phases)
-      ];
-    };
+  # Enable scx_bpfland scheduler
+  services.scx = {
+    enable = true;
+    scheduler = "scx_bpfland";
+    # Optional: Set extra options for the scheduler
+    extraArgs = [
+      "--slice-us-lag"
+      "-5000" # Negative lag for more consistent performance (reduces responsiveness but smooths long-running tasks)
+      "--primary-domain"
+      "performance" # Prioritize fastest cores initially (e.g., for bursty compilation phases)
+    ];
+  };
 }
