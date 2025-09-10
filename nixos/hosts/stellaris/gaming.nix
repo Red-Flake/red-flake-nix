c6accc9807a146515830828a2b2066575912bd57
Mag1cByt3s <ppeinecke@protonmail.com>
2025-09-10 19:23:46 +0200

    launch steam with STEAM_FORCE_DESKTOPUI_SCALING=2

--- nixos/hosts/stellaris/gaming.nix
+++ nixos/hosts/stellaris/gaming.nix
@@ -1,4 +1,4 @@
-{ 
+{
   config,
   inputs,
   pkgs,
@@ -8,9 +8,11 @@
   modulesPath,
   user,
   ...
-}: let 
+}:
+let
   gaming = inputs.nix-gaming;
-in {
+in
+{
   imports = with gaming.nixosModules; [
     # https://github.com/fufexan/nix-gaming#platform-optimizations
     pipewireLowLatency
@@ -56,22 +58,26 @@ in {
           RADV_DEBUG = "novrsflatshading";
           RADV_PERFTEST = "nggc,sam,gpl";
           DXVK_ASYNC = "1";
-        } // (prev.extraEnv or {});
+          STEAM_FORCE_DESKTOPUI_SCALING = "2";
+        }
+        // (prev.extraEnv or { });
 
-        extraLibraries = pkgs: (with pkgs; [
-          atk
-          libGLU
-          sdl2-compat
-        ]) ++ (prev.extraLibraries or []);
+        extraLibraries =
+          pkgs:
+          (with pkgs; [
+            atk
+            libGLU
+            sdl2-compat
+          ])
+          ++ (prev.extraLibraries or [ ]);
       })
     );
   };
 
-
   # https://nixos.wiki/wiki/Games
   # Adding programs.nix-ld = { enable = true; libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs; }; to your configuration to run nearly any binary by including all of the libraries used by Steam. (https://old.reddit.com/r/NixOS/comments/1d1nd9l/walking_through_why_precompiled_hello_world/)
   # FIX: error: expected a set but found a list: https://discourse.nixos.org/t/programs-nix-ld-libraries-expects-set-instead-of-list/56009/3?utm_source=chatgpt.com
-  programs.nix-ld = { 
+  programs.nix-ld = {
     enable = true;
     libraries = pkgs.steam-run.args.multiPkgs pkgs;
   };
@@ -88,12 +94,12 @@ in {
     enableRenice = true;
     settings = {
       general = {
-        softrealtime = "auto";  # Enables SCHED_RR for low latency
-        renice = 15;  # Higher priority for game processes
+        softrealtime = "auto"; # Enables SCHED_RR for low latency
+        renice = 15; # Higher priority for game processes
       };
       gpu = {
-        apply_gpu_optimisations = "accept-responsibility";  # Allow GPU tweaks
-        gpu_device = 2;  # Use discrete GPU (1 = integrated, 2 = discrete); see: => /sys/class/drm/card2/device/
+        apply_gpu_optimisations = "accept-responsibility"; # Allow GPU tweaks
+        gpu_device = 2; # Use discrete GPU (1 = integrated, 2 = discrete); see: => /sys/class/drm/card2/device/
       };
       custom = {
         start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
@@ -109,9 +115,9 @@ in {
     MANGOHUD = "0";
     OBS_VKCAPTURE = "1";
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-    WINENTSYNC = "1";  # Enable ntsync for best compatibility/performance
-    WINEESYNC = "0";   # Disable esync to avoid conflicts
-    WINEFSYNC = "0";   # Disable fsync to avoid conflicts
-    DXVK_ASYNC = "1";   # Display frames even if they are not completely rendered. This will reduce stuttering a lot, but it could theoretically trigger anti cheat, even though this never actually happened. Your DXVK version needs to be compatible or patched to use it. Proton-GE, until version 7-44, is compatible. For Non-Steam games you can't use Proton, and need a patched DXVK-Version. For Lutris you need to copy it to ~/.local/share/lutris/runtime/dxvk/, and manually select the version inside Lutris (if you named the folder dxvk-async-1.3, you also need to manually type dxvk-async-1.3 in the field). 
+    WINENTSYNC = "1"; # Enable ntsync for best compatibility/performance
+    WINEESYNC = "0"; # Disable esync to avoid conflicts
+    WINEFSYNC = "0"; # Disable fsync to avoid conflicts
+    DXVK_ASYNC = "1"; # Display frames even if they are not completely rendered. This will reduce stuttering a lot, but it could theoretically trigger anti cheat, even though this never actually happened. Your DXVK version needs to be compatible or patched to use it. Proton-GE, until version 7-44, is compatible. For Non-Steam games you can't use Proton, and need a patched DXVK-Version. For Lutris you need to copy it to ~/.local/share/lutris/runtime/dxvk/, and manually select the version inside Lutris (if you named the folder dxvk-async-1.3, you also need to manually type dxvk-async-1.3 in the field).
   };
 }
