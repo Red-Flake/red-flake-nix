99b598fcfbe82d1a6bc2e46621c0a405c71c5ae8
Mag1cByt3s <ppeinecke@protonmail.com>
2025-10-13 09:19:03 +0200

    override neo4j package and config to 4.4.11 for BloodHound CE compatibility

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -7,20 +7,48 @@
 }:
 
 let
-  # use old version of neo4j due to incompatbility with gds; use: https://lazamar.co.uk/nix-versions/
-  # get sha256 from rev using: nix-prefetch-git https://github.com/NixOS/nixpkgs/ 882842d2a908700540d206baa79efb922ac1c33d
-  # https://lazamar.co.uk/nix-versions/?package=neo4j&version=5.24.2&fullName=neo4j-5.24.2&keyName=neo4j&revision=882842d2a908700540d206baa79efb922ac1c33d&channel=nixpkgs-unstable#instructions
+  # We need to use the Neo4j LTS version 4.4.11 because In Neo4j 5.x the legacy procedure CALL db.indexes() was removed (replaced by SHOW INDEXES). BloodHound CE 8 still calls db.indexes, so it expects Neo4j 4.4.x.
+  # See: https://github.com/SpecterOps/BloodHound/blob/03454913830fec12eebc4451dca8af8b3b3c44d7/tools/docker-compose/neo4j.Dockerfile#L17
+  # Neo4j 4.4.11 is the latest Neo4j build available through https://lazamar.co.uk/nix-versions/
+  # get sha256 from rev using: `nix-prefetch-git https://github.com/NixOS/nixpkgs/ 7a339d87931bba829f68e94621536cad9132971a`
+  # https://lazamar.co.uk/nix-versions/?package=neo4j&version=4.4.11&fullName=neo4j-4.4.11&keyName=neo4j&revision=7a339d87931bba829f68e94621536cad9132971a&channel=nixpkgs-unstable#instructions
   revpkgs =
     import
       (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/882842d2a908700540d206baa79efb922ac1c33d.tar.gz";
-        sha256 = "105v2h9gpaxq6b5035xb10ykw9i3b3k1rwfq4s6inblphiz5yw7q";
+        url = "https://github.com/NixOS/nixpkgs/archive/7a339d87931bba829f68e94621536cad9132971a.tar.gz";
+        sha256 = "1w4zyrdq7zjrq2g3m7bhnf80ma988g87m7912n956md8fn3ybhr4";
       })
       {
         system = "x86_64-linux"; # Set your system explicitly
       };
 
-  neo4j_5_24_2 = revpkgs.neo4j;
+  neo4j_4_4_11 = revpkgs.neo4j;
+
+  neo4j44Conf = pkgs.writeText "neo4j-4.4.conf" ''
+    # Neo4j 4.4 style config (no server.* keys)
+
+    # Listen on localhost (or 0.0.0.0 if you prefer)
+    dbms.default_listen_address=127.0.0.1
+
+    # Bolt
+    dbms.connector.bolt.enabled=true
+    dbms.connector.bolt.listen_address=:7687
+    dbms.connector.bolt.tls_level=DISABLED
+
+    # HTTP
+    dbms.connector.http.enabled=true
+    dbms.connector.http.listen_address=:7474
+
+    # (optional) HTTPS
+    # dbms.connector.https.enabled=false
+    # dbms.connector.https.listen_address=:7473
+
+    # GDS permissions
+    dbms.security.procedures.unrestricted=gds.*
+    dbms.security.procedures.allowlist=gds.*
+
+    # Logs/data/run live under /var/lib/neo4j
+  '';
 in
 {
   # ZFS services
@@ -80,12 +108,18 @@ in
   # Neo4j settings
   services.neo4j = {
     enable = true;
-    package = neo4j_5_24_2;
+
+    # set package to 4.4.11 for BloodHound CE compatibility
+    package = neo4j_4_4_11;
+
+    # set neo4j directories to /var/lib/neo4j
     directories.home = lib.mkForce "/var/lib/neo4j";
     directories.data = lib.mkForce "/var/lib/neo4j/data";
     directories.plugins = lib.mkForce "/var/lib/neo4j/plugins";
     directories.imports = lib.mkForce "/var/lib/neo4j/import";
     directories.certificates = lib.mkForce "/var/lib/neo4j/certificates";
+
+    # setup listeners
     https.sslPolicy = "legacy";
     http.listenAddress = ":7474";
     https.listenAddress = ":7473";
@@ -94,8 +128,17 @@ in
     bolt.listenAddress = ":7687";
     bolt.enable = true;
     https.enable = false;
-    extraServerConfig = "dbms.security.procedures.unrestricted=gds.*\ndbms.security.procedures.allowlist=gds.*\n";
+
+    # IMPORTANT: use 4.4 keys and allow gds procedures
+    extraServerConfig = ""; # we’re supplying the whole file ourselves
   };
+
+  # Force our own preStart so the neo4j module does NOT link its generated server.* file.
+  systemd.services.neo4j.preStart = lib.mkForce ''
+    set -eu
+    install -d -m 0700 -o neo4j -g neo4j /var/lib/neo4j/{conf,logs,run,plugins,import,data}
+    install -m 0600 -o neo4j -g neo4j ${neo4j44Conf} /var/lib/neo4j/conf/neo4j.conf
+  '';
 
   # BloodHound-CE service settings
   services.bloodhound-ce = {
@@ -117,6 +160,8 @@ in
       # passwordFile = "/run/secrets/bh-db.env"; # bhe_database_secret=...
     };
 
+    # We need to use the Neo4j LTS version 4.4.x because In Neo4j 5.x the legacy procedure CALL db.indexes() was removed (replaced by SHOW INDEXES). BloodHound CE 8 still calls db.indexes, so it expects Neo4j 4.4.x.
+    # See: https://github.com/SpecterOps/BloodHound/blob/03454913830fec12eebc4451dca8af8b3b3c44d7/tools/docker-compose/neo4j.Dockerfile#L17
     neo4j = {
       host = "127.0.0.1";
       port = 7687;
--- nixos/modules/setup-neo4j.nix
+++ nixos/modules/setup-neo4j.nix
@@ -1,26 +1,45 @@
-{ pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+# setup correct supported Neo4j GDS plugin for the correct neo4j version
+# see: https://neo4j.com/docs/graph-data-science/current/installation/supported-neo4j-versions/
+# use gds version 2.6.x for neo4j 4.4.11
+# lets go with latest 2.6.x version: 2.6.8 (2024-06-28)
+
+# Match GDS to Neo4j 4.4.x
 let
-  gdsVersion = "2.12.0";
+  neo4jPkg = config.services.neo4j.package; # <- uses your pinned 4.4.11
+  gdsVersion = "2.6.8"; # compatible with 4.4.x
   gdsJarName = "neo4j-graph-data-science-${gdsVersion}.jar";
-  gdsDownloadUrl = "https://github.com/neo4j/graph-data-science/releases/download/${gdsVersion}/${gdsJarName}";
+  gdsJar = pkgs.fetchurl {
+    url = "https://github.com/neo4j/graph-data-science/releases/download/${gdsVersion}/${gdsJarName}";
+    sha256 = "sha256-hzEakrAEUHsEOm0u9i6pbzemwKrbWJGqcY6ZGLip5Uk=";
+  };
 in
 {
-  system.activationScripts.setup-neo4j = {
-    text = ''
-      plugins_dir="/var/lib/neo4j/plugins"
+  system.activationScripts.setup-neo4j.text = ''
+    set -eu
 
-      if [ ! -d "$plugins_dir" ]; then
-        mkdir -p "$plugins_dir"
-      fi
+    # Ensure dirs owned by neo4j
+    install -d -m 0700 -o neo4j -g neo4j /var/lib/neo4j/{data,plugins,conf}
+    install -d -m 0700 -o neo4j -g neo4j /var/lib/neo4j/data/dbms
 
-      if [ ! -f "$plugins_dir/${gdsJarName}" ]; then
-        echo "Downloading Graph Data Science ${gdsVersion} plugin..."
-        ${lib.getExe' pkgs.wget "wget"} -O "$plugins_dir/${gdsJarName}" "${gdsDownloadUrl}"
-      fi
+    # Install (symlink) GDS plugin immutably
+    if [ ! -e /var/lib/neo4j/plugins/${gdsJarName} ]; then
+      ln -sfn ${gdsJar} /var/lib/neo4j/plugins/${gdsJarName}
+      chown -h neo4j:neo4j /var/lib/neo4j/plugins/${gdsJarName}
+    fi
 
-      # Set initial Neo4j password
-      NEO4J_CONF=/var/lib/neo4j/conf ${lib.getExe' pkgs.neo4j "neo4j-admin"} dbms set-initial-password Password1337
-    '';
-  };
+    # Set initial password only once (before first start)
+    if [ ! -f /var/lib/neo4j/data/dbms/auth.ini ]; then
+      echo "Setting initial Neo4j password…"
+      su -s /bin/sh -c \
+        'NEO4J_HOME=/var/lib/neo4j NEO4J_CONF=/var/lib/neo4j/conf ${lib.getExe' neo4jPkg "neo4j-admin"} set-initial-password "Password1337"' \
+        neo4j || true
+    fi
+  '';
 }
