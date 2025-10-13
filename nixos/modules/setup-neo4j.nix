7b786c69d6d5ca446d8d617a1c1626f5d9e01805
Mag1cByt3s <ppeinecke@protonmail.com>
2025-10-13 11:23:23 +0200

    use correct APOC version 4.4.0.24 for neo4j version 4.4.11

--- nixos/modules/setup-neo4j.nix
+++ nixos/modules/setup-neo4j.nix
@@ -5,32 +5,35 @@
   ...
 }:
 
+# BloodHound-CE needs Neo4j 4.4.11 for compatibility
+
 # setup correct supported Neo4j GDS plugin for the correct neo4j version
 # see: https://neo4j.com/docs/graph-data-science/current/installation/supported-neo4j-versions/
 # use gds version 2.6.x for neo4j 4.4.11
 # lets go with latest 2.6.x version: 2.6.8 (2024-06-28)
 
-# also install apoc-4.4.0.33-core.jar plugin
-# from: https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/tag/4.4.0.33
+# also install apoc-4.4.0.24-core.jar plugin for neo4j 4.4.11
+# for Neo4j 4.4.11, the APOC docs’ matrix says to use APOC 4.4.0.24 for all 4.4.x releases. The newer APOC jars (e.g. 4.4.0.33/35) call Config.expandCommands() which doesn’t exist in 4.4.11.
+# from: https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/tag/4.4.0.24
 # see: https://github.com/SpecterOps/BloodHound/blob/03454913830fec12eebc4451dca8af8b3b3c44d7/tools/docker-compose/neo4j.Dockerfile#L28
 # see: https://neo4j.com/docs/graph-data-science/current/installation/supported-neo4j-versions/
 
-# Match GDS & APOC to Neo4j 4.4.x
+# Match GDS & APOC to Neo4j 4.4.11
 let
   neo4jPkg = config.services.neo4j.package; # <- uses your pinned 4.4.11
 
-  gdsVersion = "2.6.8"; # compatible with 4.4.x
+  gdsVersion = "2.6.8"; # compatible with 4.4.11
   gdsJarName = "neo4j-graph-data-science-${gdsVersion}.jar";
   gdsJar = pkgs.fetchurl {
     url = "https://github.com/neo4j/graph-data-science/releases/download/${gdsVersion}/${gdsJarName}";
     sha256 = "sha256-hzEakrAEUHsEOm0u9i6pbzemwKrbWJGqcY6ZGLip5Uk=";
   };
 
-  apocVersion = "4.4.0.33"; # as being compatible with 4.4.x
+  apocVersion = "4.4.0.24"; # as being compatible with 4.4.11
   apocJarName = "apoc-${apocVersion}-core.jar";
   apocJar = pkgs.fetchurl {
     url = "https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/${apocVersion}/${apocJarName}";
-    sha256 = "sha256-vgQsGjW7GeaArDOXJfA0UWQjyRNP/81ieKml+6HVYmM=";
+    sha256 = "sha256-doaN5k+i3SmKmsxWx4OcwDzeTokRbV4OAGJOl4G+Tkc=";
   };
 in
 {
