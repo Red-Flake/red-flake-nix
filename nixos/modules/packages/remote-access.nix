bc60d36d7ff575f67030c9b6a43e3d0bcf78db8d
Mag1cByt3s <ppeinecke@protonmail.com>
2025-10-20 08:43:10 +0200

    add package evil-winrm-py from redflake packages repo

--- nixos/modules/packages/remote-access.nix
+++ nixos/modules/packages/remote-access.nix
@@ -1,10 +1,18 @@
-{ inputs, config, lib, pkgs, modulesPath, ... }:
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     evil-winrm-patched
+    inputs.redflake-packages.packages.x86_64-linux.evil-winrm-py
     openssh
     freerdp3
     rdesktop
