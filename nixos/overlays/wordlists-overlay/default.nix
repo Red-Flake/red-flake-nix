2ca0af9f5fc84dbf41e49c8e8fbe64b9e56591ef
Mag1cByt3s <ppeinecke@protonmail.com>
2025-11-14 07:49:57 +0100

    updated tools input + wordlists overlay

--- nixos/overlays/wordlists-overlay/default.nix
+++ nixos/overlays/wordlists-overlay/default.nix
@@ -6,13 +6,16 @@ self: super: {
     # Fetch the repository using fetchgit with Git LFS support.
     src = super.fetchgit {
       url = "https://github.com/Red-Flake/wordlists.git";
-      rev = "a64e96a5685ff5e364ce067dd27dc551a4e9234c"; # e.g. commit hash or branch name
-      sha256 = "sha256-XU3eL5O8NyUYWnplQc65E6b1gQhR/Z80tZMBr9DIMOk=";
+      rev = "2a5f870fb6bb7880571bcbf66165295f8c31dba7"; # e.g. commit hash or branch name
+      sha256 = "sha256-WisOCYFR8zUkKjU3XTj4LHqszqJ0P2Vpk6FIADT4rpo=";
       fetchLFS = true;
     };
 
     # We only need the install phase since the repo is already unpacked.
-    phases = [ "unpackPhase" "installPhase" ];
+    phases = [
+      "unpackPhase"
+      "installPhase"
+    ];
     installPhase = ''
       mkdir -p $out/share/wordlists
       cp -r * $out/share/wordlists
