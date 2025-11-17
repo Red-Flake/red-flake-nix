f7269498a62fc542f89a5186e7d5eda9ae6db189
Mag1cByt3s <ppeinecke@protonmail.com>
2025-11-17 09:38:26 +0100

    updated wordlists overlay; added fasttrack.txt

--- nixos/overlays/wordlists-overlay/default.nix
+++ nixos/overlays/wordlists-overlay/default.nix
@@ -6,16 +6,16 @@ self: super: {
     # Fetch the repository using fetchgit with Git LFS support.
     src = super.fetchgit {
       url = "https://github.com/Red-Flake/wordlists.git";
-      rev = "3f20a0b27f0574d1937a324826659d7bd4d705bf"; # e.g. commit hash or branch name
-      sha256 = "sha256-fTuj7FAOGWnfKTekIex3ZCf3VBx4OSqVpAV3Vrgt+Ek=";
+      rev = "4527384f1d0e919fa89d8d613be23c31122ee2aa"; # e.g. commit hash or branch name
+      sha256 = "sha256-UkqkfbJrWBjjly4Pbm4rXA1oGJc5kUGUZHgoH3iyL14=";
       fetchLFS = true;
     };
 
-    # We only need the install phase since the repo is already unpacked.
     phases = [
       "unpackPhase"
       "installPhase"
     ];
+
     installPhase = ''
       mkdir -p $out/share/wordlists
       cp -r * $out/share/wordlists
