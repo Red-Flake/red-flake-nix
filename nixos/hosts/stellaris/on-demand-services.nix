e2cb8d509376bf0662c7be76f8b2afae57aa72c5
Mag1cByt3s <ppeinecke@protonmail.com>
2026-02-20 08:42:54 +0100

    re-enable services except ollama by default again

--- nixos/hosts/stellaris/on-demand-services.nix
+++ nixos/hosts/stellaris/on-demand-services.nix
@@ -3,12 +3,12 @@
   # Keep these services installed/configured, but don't start them automatically.
   # Start them when needed via `sudo systemctl start <name>`.
   systemd.services = {
-    bloodhound-ce.wantedBy = lib.mkForce [ ];
-    neo4j.wantedBy = lib.mkForce [ ];
-    postgresql.wantedBy = lib.mkForce [ ];
+    #bloodhound-ce.wantedBy = lib.mkForce [ ];
+    #neo4j.wantedBy = lib.mkForce [ ];
+    #postgresql.wantedBy = lib.mkForce [ ];
 
-    docker.wantedBy = lib.mkForce [ ];
-    containerd.wantedBy = lib.mkForce [ ];
+    #docker.wantedBy = lib.mkForce [ ];
+    #containerd.wantedBy = lib.mkForce [ ];
 
     ollama.wantedBy = lib.mkForce [ ];
   };
