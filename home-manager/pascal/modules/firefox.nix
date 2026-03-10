03185dc38a1f5ae0763c4e995d0ae649e2508211
Mag1cByt3s <ppeinecke@protonmail.com>
2026-03-10 04:27:55 +0100

    firefox: fully disable AI features

--- home-manager/pascal/modules/firefox.nix
+++ home-manager/pascal/modules/firefox.nix
@@ -316,6 +316,14 @@
         "browser.ai.control.smartTabGroups" = "blocked";
         "browser.ai.control.translations" = "blocked";
         "browser.aiwindow.enabled" = false;
+        "browser.ml.chat.enabled" = false;
+        "browser.ml.chat.page" = false;
+        "browser.ml.linkPreview.enabled" = false;
+        "browser.tabs.groups.smart.enabled" = false;
+        "browser.tabs.groups.smart.userEnabled" = false;
+        "browser.translations.enable" = false;
+        "extensions.ml.enabled" = false;
+        "pdfjs.enableAltText" = false;
       };
 
       extraConfig = ''
